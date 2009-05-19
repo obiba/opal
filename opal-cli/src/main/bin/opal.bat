e129b14b2ba4118e10bb8c8929f3375846cc67cd
Tony DeBat <tony.debat@oicr.on.ca>
2009-05-19 17:10:14 +0000

    OPAL-7
    - Updated scripts to use JAVA_HOME and OPAL_HOME.

--- opal-cli/src/main/bin/opal.bat
+++ opal-cli/src/main/bin/opal.bat
@@ -1,2 +1,19 @@
 @echo off
-java -Djava.ext.dirs=..\lib org.obiba.opal.cli.client.impl.OpalClient %*
+set JAVA_HOME=
+set OPAL_HOME=
+
+if "%JAVA_HOME%" == "" goto JAVA_HOME_NOT_SET
+if "%OPAL_HOME%" == "" goto OPAL_HOME_NOT_SET
+
+java -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%OPAL_HOME%\lib" -cp "%OPAL_HOME%\conf" org.obiba.opal.cli.client.impl.OpalClient %*
+goto :END
+
+:JAVA_HOME_NOT_SET
+echo JAVA_HOME not set
+exit
+
+:OPAL_HOME_NOT_SET
+echo OPAL_HOME not set
+exit
+
+:END
