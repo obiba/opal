fe0c746eeb0158fae7f8e175d3994768829d4358
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-08-20 16:05:37 +0000

    OPAL-50: Modified MySQLSchemaChangeSqlVisitor to specify create either InnoDB or MyISAM tables, based on the numbers of columns in the table (> 1000 = MyISAM).

--- jdbc-mart/src/main/java/org/obiba/opal/jdbcmart/batch/MySQLSchemaChangeSqlVisitor.java
+++ jdbc-mart/src/main/java/org/obiba/opal/jdbcmart/batch/MySQLSchemaChangeSqlVisitor.java
@@ -1,6 +1,8 @@
 package org.obiba.opal.jdbcmart.batch;
 
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import liquibase.database.Database;
 import liquibase.database.sql.visitor.SqlVisitor;
@@ -10,7 +12,13 @@ public class MySQLSchemaChangeSqlVisitor implements SqlVisitor {
   // Constants
   //
 
-  public static final String CREATE_TABLE_SUFFIX = " ENGINE=InnoDB DEFAULT CHARSET=latin1";
+  public static final String CREATE_INNODB_TABLE_SUFFIX = " ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+  public static final String CREATE_MYISAM_TABLE_SUFFIX = " ENGINE=MyISAM DEFAULT CHARSET=latin1";
+
+  public static final int MAX_INNODB_COLUMNS = 1000;
+
+  private static final String MART_COLUMN_PREFIX = "OPAL_";
 
   //
   // SqlVisitor Methods
@@ -30,11 +38,40 @@ public class MySQLSchemaChangeSqlVisitor implements SqlVisitor {
   }
 
   public String modifySql(String sql, Database database) {
-    // Append suffix indicating InnoDB and latin1 charset.
     if(sql.toUpperCase().startsWith("CREATE TABLE")) {
-      sql += CREATE_TABLE_SUFFIX;
+      int columnCount = getColumnCount(sql);
+
+      if(columnCount > MAX_INNODB_COLUMNS) {
+        sql += CREATE_MYISAM_TABLE_SUFFIX;
+      } else {
+        sql += CREATE_INNODB_TABLE_SUFFIX;
+      }
     }
 
     return sql;
+  }
+
+  //
+  // Methods
+  //
+
+  /**
+   * Returns the number of columns in the specified table.
+   * 
+   * @param createTableSql table creation SQL
+   * @return columns in the table (1 entity_id column + N columns named "OPAL_*")
+   */
+  public int getColumnCount(String createTableSql) {
+    Pattern p = Pattern.compile(MART_COLUMN_PREFIX);
+    Matcher m = p.matcher(createTableSql);
+
+    // Start columnCount at 1 (there is at least the entity_id column).
+    int columnCount = 1;
+
+    while(m.find()) {
+      columnCount++;
+    }
+
+    return columnCount;
   }
 }
