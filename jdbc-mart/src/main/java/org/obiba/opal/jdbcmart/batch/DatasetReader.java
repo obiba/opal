ade1a74051f2596aa46f7635b9df3485ca0fafb5
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2009-08-24 16:24:53 +0000

    OPAL-55

--- jdbc-mart/src/main/java/org/obiba/opal/jdbcmart/batch/DatasetReader.java
+++ jdbc-mart/src/main/java/org/obiba/opal/jdbcmart/batch/DatasetReader.java
@@ -1,16 +1,49 @@
 package org.obiba.opal.jdbcmart.batch;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.obiba.opal.batch.StartDateIncrementer;
 import org.obiba.opal.core.domain.data.Dataset;
 import org.springframework.batch.item.database.HibernateCursorItemReader;
 
 public class DatasetReader extends HibernateCursorItemReader<Dataset> {
+  //
+  // Constants
+  //
 
-  private Date startDate;
+  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-  public void setStartDate(Date startDate) {
-    this.startDate = startDate;
+  //
+  // Instance Variables
+  //
+
+  private Map<String, Object> jobParameters;
+
+  //
+  // Constructors
+  //
+
+  public DatasetReader() {
+    jobParameters = new LinkedHashMap<String, Object>();
+  }
+
+  //
+  // HibernateCursorItemReader Methods
+  //
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    super.afterPropertiesSet();
+
+    Date startDate = (Date) jobParameters.get(StartDateIncrementer.START_DATE_KEY);
+    if(startDate != null) {
+      super.setQueryString("from Dataset d where d.creationDate >= '" + DATE_FORMAT.format(startDate) + "'");
+    } else {
+      super.setQueryString("from Dataset d");
+    }
   }
 
   @Override
@@ -18,4 +51,13 @@ public class DatasetReader extends HibernateCursorItemReader<Dataset> {
     super.doOpen();
   }
 
+  //
+  // Methods
+  //
+
+  public void setJobParameters(Map<String, Object> jobParameters) {
+    if(jobParameters != null) {
+      this.jobParameters.putAll(jobParameters);
+    }
+  }
 }
---
+++ opal-datasource/src/main/java/org/obiba/opal/batch/StartDateIncrementer.java
@@ -0,0 +1,53 @@
+package org.obiba.opal.batch;
+
+import java.util.Map;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+
+public class StartDateIncrementer implements JobParametersIncrementer {
+  //
+  // Constants
+  //
+
+  public static final String START_DATE_KEY = "startDate";
+
+  //
+  // Instance Variables
+  //
+
+  private JobRepository jobRepository;
+
+  private String jobName;
+
+  //
+  // JobParametersIncrementer Methods
+  //
+
+  public JobParameters getNext(JobParameters parameters) {
+    JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, parameters);
+    if(lastJobExecution != null && lastJobExecution.getStartTime() != null) {
+      Map<String, JobParameter> parameterMap = parameters.getParameters();
+      JobParameter startDate = new JobParameter(lastJobExecution.getStartTime());
+      parameterMap.put(START_DATE_KEY, startDate);
+      return new JobParameters(parameterMap);
+    }
+
+    return parameters;
+  }
+
+  //
+  // Methods
+  //
+
+  public void setJobRepository(JobRepository jobRepository) {
+    this.jobRepository = jobRepository;
+  }
+
+  public void setJobName(String jobName) {
+    this.jobName = jobName;
+  }
+}
