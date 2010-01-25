ea71cccdb8912dcd1285384d7d3404fc1484e966
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-01-25 20:14:34 +0000

    Changed upgrade management according to COM-10 modifications.

--- opal-cli/src/main/java/org/obiba/opal/cli/client/command/UpgradeCommand.java
+++ opal-cli/src/main/java/org/obiba/opal/cli/client/command/UpgradeCommand.java
@@ -9,19 +9,11 @@
  ******************************************************************************/
 package org.obiba.opal.cli.client.command;
 
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.obiba.opal.cli.client.command.options.UpgradeCommandOptions;
 import org.obiba.runtime.upgrade.UpgradeException;
 import org.obiba.runtime.upgrade.UpgradeManager;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.support.DatabaseMetaDataCallback;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.MetaDataAccessException;
 
 /**
  * Command to perform an upgrade (i.e., invoke the upgrade manager).
@@ -44,47 +36,15 @@ public class UpgradeCommand extends AbstractContextLoadingCommand<UpgradeCommand
   public void executeWithContext() {
     new UserAuthentication(options).authenticate();
 
-    String runtimeDatabase = getRuntimeDatabase();
-
-    if(supportsDatabase(runtimeDatabase)) {
-      UpgradeManager upgradeManager = getBean("upgradeManager");
-      try {
-        upgradeManager.executeUpgrade();
-      } catch(UpgradeException upgradeFailed) {
-        throw new RuntimeException("An error occurred while running the upgrade manager", upgradeFailed);
-      }
-    } else {
-      throw new RuntimeException("Upgrade manager does not support database (" + runtimeDatabase + ")");
+    UpgradeManager upgradeManager = getBean("upgradeManager");
+    try {
+      upgradeManager.executeUpgrade();
+    } catch(UpgradeException upgradeFailed) {
+      throw new RuntimeException("An error occurred while running the upgrade manager", upgradeFailed);
     }
   }
 
   protected ConfigurableApplicationContext loadContext() {
     return new ClassPathXmlApplicationContext(CONTEXT_PATHS);
-  }
-
-  //
-  // Methods
-  //
-
-  private boolean supportsDatabase(String runtimeDatabase) {
-    return (runtimeDatabase != null && runtimeDatabase.equals("MySQL"));
-  }
-
-  private String getRuntimeDatabase() {
-    String runtimeDatabase = null;
-
-    DataSource dataSource = getBean("opalDataSource");
-
-    try {
-      runtimeDatabase = (String) JdbcUtils.extractDatabaseMetaData(dataSource, new DatabaseMetaDataCallback() {
-        public Object processMetaData(DatabaseMetaData dbmd) throws SQLException, MetaDataAccessException {
-          return dbmd.getDatabaseProductName();
-        }
-      });
-    } catch(MetaDataAccessException ex) {
-      throw new RuntimeException("Could not determine runtime database", ex);
-    }
-
-    return runtimeDatabase;
   }
 }
