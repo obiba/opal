becdee1f0f3af713a1afb72c7bffe73f7944837c
Tony DeBat <tony.debat@oicr.on.ca>
2010-01-29 05:29:33 +0000

    OPAL-75 - Added CertificateInfo to interactively build attributes needed to create a Certificate - Provided the outline of the 'key' command - StudyKeyStoreService added to Spring/Hibernate configuration - Bouncy Castle encryption added to Maven pom. Needed to create a Certificate.

--- opal-cli/src/main/java/org/obiba/opal/cli/client/command/KeyCommand.java
+++ opal-cli/src/main/java/org/obiba/opal/cli/client/command/KeyCommand.java
@@ -9,16 +9,44 @@
  ******************************************************************************/
 package org.obiba.opal.cli.client.command;
 
+import org.obiba.opal.cli.client.command.options.CertificateInfo;
 import org.obiba.opal.cli.client.command.options.KeyCommandOptions;
+import org.obiba.opal.core.service.StudyKeyStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Provides key management allowing for key creation, deletion, importing and exporting of keys.
  */
 public class KeyCommand extends AbstractCommand<KeyCommandOptions> {
 
-  // @Override
+  @Autowired
+  private StudyKeyStoreService studyKeyStoreService;
+
   public void execute() {
-    System.out.println("The key command.");
+    if(options.isDelete()) {
+      System.out.println("delete");
+    } else if(options.isAlgorithm() && options.isSize()) {
+      System.out.println("create key");
+      // check if alias already exists. If yes, then confirm the overwrite.
+      for(String id : studyKeyStoreService.getStudyIds()) {
+        System.out.println(id);
+      }
+      CertificateInfo info = new CertificateInfo();
+      System.out.println(info.getCertificateInfoAsString());
+    } else if(options.isPrivate()) {
+      System.out.println("import");
+    } else {
+      unrecognizedOptionsHelp();
+    }
+  }
+
+  private void unrecognizedOptionsHelp() {
+    System.out.println("This combination of options was unrecognized." + "\nSyntax:" //
+        + "\n   opalkey key --alias=NAME (--delete | --algo=NAME --size=INT | --private=FILE [--certificate=FILE])" //
+        + "\nExamples:" //
+        + "\n   opalkey key --alias=onyx --algo=RSA --size=2048" //
+        + "\n   opalkey key --alias=onyx --private=private_key.pem --certificate=public_key.pem" //
+        + "\n  opalkey key -alias=onyx --delete"); //
   }
 
 }
