bd545b43cc3585682f4958dc90b188c6c5fe7a7b
Tony DeBat <tony.debat@oicr.on.ca>
2010-02-10 16:58:45 +0000

    OPAL-125 - Resolved the list of table names into a List of TableValues. Exceptions are thrown in the tables or datasources do not exist.

--- opal-core/src/main/java/org/obiba/opal/core/service/impl/DefaultExportServiceImpl.java
+++ opal-core/src/main/java/org/obiba/opal/core/service/impl/DefaultExportServiceImpl.java
@@ -10,13 +10,18 @@
 package org.obiba.opal.core.service.impl;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.obiba.magma.Datasource;
 import org.obiba.magma.MagmaEngine;
 import org.obiba.magma.NoSuchDatasourceException;
+import org.obiba.magma.NoSuchValueTableException;
+import org.obiba.magma.ValueTable;
 import org.obiba.magma.audit.hibernate.HibernateVariableEntityAuditLogManager;
+import org.obiba.magma.support.MagmaEngineReferenceResolver;
 import org.obiba.opal.core.service.ExportService;
+import org.springframework.util.Assert;
 
 /**
  * Default implementation of {@link ExportService}.
@@ -30,20 +35,35 @@ public class DefaultExportServiceImpl implements ExportService {
   }
 
   public void exportTablesToDatasource(List<String> fromTableNames, String destinationDatasourceName) {
+    Assert.notEmpty(fromTableNames, "fromTableNames must not be null or empty");
+    Assert.hasText(destinationDatasourceName, "destinationDatasourceName must not be null or empty");
     Datasource destinationDatasource = getDatasourceByName(destinationDatasourceName);
+    List<ValueTable> sourceTables = getValueTablesByName(fromTableNames);
     throw new UnsupportedOperationException("Exporting to an existing datasource destination is not currently supported.");
   }
 
   public void exportTablesToExcelFile(List<String> fromTableNames, File destinationExcelFile) {
+    Assert.notEmpty(fromTableNames, "fromTableNames must not be null or empty");
+    Assert.notNull(destinationExcelFile, "destinationExcelFile must not be null");
     throw new UnsupportedOperationException("Exporting to an Excel file is not currently supported.");
   }
 
   private Datasource getDatasourceByName(String datasourceName) {
     Datasource datasource = MagmaEngine.get().getDatasource(datasourceName);
+
     if(datasource == null) {
       throw new NoSuchDatasourceException("No such datasource '" + datasourceName + "'.");
     }
     return datasource;
+  }
+
+  private List<ValueTable> getValueTablesByName(List<String> tableNames) throws NoSuchDatasourceException, NoSuchValueTableException {
+    List<ValueTable> tables = new ArrayList<ValueTable>(tableNames.size());
+    for(String tableName : tableNames) {
+      // Resolver expects ValueTable names to be delimited with a colon.
+      tables.add(MagmaEngineReferenceResolver.valueOf(tableName + ":").resolveTable());
+    }
+    return tables;
   }
 
 }
