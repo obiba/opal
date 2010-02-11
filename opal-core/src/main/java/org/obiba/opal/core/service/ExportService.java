4089c8fd1dd9af4b204ef0bf84eaa872910ec861
Tony DeBat <tony.debat@oicr.on.ca>
2010-02-11 22:21:07 +0000

    OPAL-125 - Added JavaDoc to the ExportService methods.

--- opal-core/src/main/java/org/obiba/opal/core/service/ExportService.java
+++ opal-core/src/main/java/org/obiba/opal/core/service/ExportService.java
@@ -15,6 +15,7 @@ import java.util.Set;
 
 import org.obiba.magma.Datasource;
 import org.obiba.magma.NoSuchDatasourceException;
+import org.obiba.magma.NoSuchValueTableException;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.support.DatasourceCopier;
 
@@ -24,21 +25,45 @@ import org.obiba.magma.support.DatasourceCopier;
 public interface ExportService {
 
   /**
-   * Export tables to an existing Datasource.
+   * Export tables to an existing Datasource. This export operation will be logged.
    * @param sourceTableNames tables to export.
    * @param destinationDatasourceName tables will be copied to this existing Datasource.
    * @throws NoSuchDatasourceException if the destinationDatasourceName does not exist.
+   * @throws NoSuchValueTableException if a sourceTableName does not exist.
+   * @throws ExportException if the sourceTableNames are not unique or if the datasource of a sourceTableName matches
+   * the destinationDatasource.
    */
   public void exportTablesToDatasource(List<String> sourceTableNames, String destinationDatasourceName);
 
+  /**
+   * Export tables to an existing Datasource using the provided {@link DatasourceCopier}. If logging is required ensure
+   * that the {@code DatasourceCopier} is configured with an appropriate logger.
+   * @param sourceTableNames tables to export.
+   * @param destinationDatasourceName tables will be copied to this existing Datasource.
+   * @param datasourceCopier copier used to perform the copy.
+   * @throws NoSuchDatasourceException if the destinationDatasourceName does not exist.
+   * @throws NoSuchValueTableException if a sourceTableName does not exist.
+   * @throws ExportException if the sourceTableNames are not unique or if the datasource of a sourceTableName matches
+   * the destinationDatasource.
+   */
   public void exportTablesToDatasource(List<String> sourceTableNames, String destinationDatasourceName, DatasourceCopier datasourceCopier);
 
+  /**
+   * Export tables to the provided {@link Datasource} using the provided {@link DatasourceCopier}. If logging is
+   * required ensure that the {@code DatasourceCopier} is configured with an appropriate logger. It is the
+   * responsibility of the caller to remove the {@code destinationDatasource} from {@code Magama}.
+   * @param sourceTables tables to export.
+   * @param destinationDatasource tables will be copied to this existing Datasource.
+   * @param datasourceCopier copier used to perform the copy.
+   * @throws ExportException if the datasource of a sourceTable matches the destinationDatasource.
+   */
   public void exportTablesToDatasource(Set<ValueTable> sourceTables, Datasource destinationDatasource, DatasourceCopier datasourceCopier);
 
   /**
-   * Export tables to an Excel file.
+   * Export tables to an Excel file. This export operation will be logged.
    * @param sourceTableNames tables to export.
    * @param destinationExcelFilename tables will be copied to this Excel file.
+   * @throws UnsupportedOperationException Exporting to an Excel file is not currently implemented.
    */
   public void exportTablesToExcelFile(List<String> sourceTableNames, File destinationExcelFile);
 
