499b45641b312ea9e2b596f1efd0481ee3536254
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-03-18 21:15:59 +0000

    Removed obsolete OpalKeystore, StudyKeyStore, StudyKeyStoreService and related artifacts.

--- opal-core/src/main/java/org/obiba/opal/core/support/OpalAnnotationConfigurationHelper.java
+++ opal-core/src/main/java/org/obiba/opal/core/support/OpalAnnotationConfigurationHelper.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-import org.obiba.opal.core.crypt.StudyKeyStore;
 import org.obiba.opal.core.domain.unit.UnitKeyStoreState;
 import org.springframework.beans.factory.FactoryBean;
 
@@ -13,8 +12,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 public class OpalAnnotationConfigurationHelper implements FactoryBean {
-  // TODO: Remove StudyKeyStore.class for annotatedClasses.
-  private final Set<Class<?>> annotatedClasses = new ImmutableSet.Builder<Class<?>>().add(StudyKeyStore.class).add(UnitKeyStoreState.class).build();
+  private final Set<Class<?>> annotatedClasses = new ImmutableSet.Builder<Class<?>>().add(UnitKeyStoreState.class).build();
 
   private final List<Class<?>> additionalClasses = Lists.newArrayList();
 
