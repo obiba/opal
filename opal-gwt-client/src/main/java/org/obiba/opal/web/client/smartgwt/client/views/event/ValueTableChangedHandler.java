8390bb87202c6562b258d2a79c2b7df499f96b01
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-05-14 03:04:10 +0000

    Removing SmartGWT stuff

--- opal-gwt-client/src/main/java/org/obiba/opal/web/client/smartgwt/client/views/event/ValueTableChangedHandler.java
+++
@@ -1,7 +0,0 @@
-package org.obiba.opal.web.client.smartgwt.client.views.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ValueTableChangedHandler extends EventHandler {
-  void onValueTableChanged(ValueTableChangedEvent event);
-}
--- opal-gwt-client/src/main/java/org/obiba/opal/web/client/smartgwt/client/views/event/VariableChangedHandler.java
+++
@@ -1,7 +0,0 @@
-package org.obiba.opal.web.client.smartgwt.client.views.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface VariableChangedHandler extends EventHandler {
-  void onVariableChanged(VariableChangedEvent event);
-}
