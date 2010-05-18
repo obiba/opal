7f9425a8d907ffbaec00268199026ef4ff0fea47
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-05-18 22:07:22 +0000

    OPAL-331 * Fixed PMD warning

--- opal-ws/src/main/java/org/obiba/opal/web/ws/provider/ProtobufJsonReaderProvider.java
+++ opal-ws/src/main/java/org/obiba/opal/web/ws/provider/ProtobufJsonReaderProvider.java
@@ -17,6 +17,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
@@ -47,6 +49,8 @@ public class ProtobufJsonReaderProvider implements MessageBodyReader<Object> {
 
   private static final Logger log = LoggerFactory.getLogger(ProtobufJsonReaderProvider.class);
 
+  private final BuilderFactory builderFactory = new BuilderFactory();
+
   @Override
   public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
     return Message.class.isAssignableFrom(type) || isWrapped(type, genericType, annotations, mediaType);
@@ -56,26 +60,7 @@ public class ProtobufJsonReaderProvider implements MessageBodyReader<Object> {
   public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
     Class<Message> messageType = extractMessageType(type, genericType, annotations, mediaType);
 
-    final Builder builder;
-    try {
-      Method method = messageType.getMethod("newBuilder");
-      // newBuilder() is static
-      builder = (Builder) method.invoke(null);
-    } catch(SecurityException e) {
-      log.error("Error getting 'newBuilder' method from type " + type.getName(), e);
-      throw new WebApplicationException(500);
-    } catch(NoSuchMethodException e) {
-      throw new IllegalStateException("The Message type " + type.getName() + " does not define a 'newBuilder' static method.");
-    } catch(IllegalArgumentException e) {
-      log.error("Error invoking 'newBuilder' method for type " + type.getName(), e);
-      throw new WebApplicationException(500);
-    } catch(IllegalAccessException e) {
-      log.error("Error invoking 'newBuilder' method for type " + type.getName(), e);
-      throw new WebApplicationException(500);
-    } catch(InvocationTargetException e) {
-      log.error("Error invoking 'newBuilder' method for type " + type.getName(), e);
-      throw new WebApplicationException(500);
-    }
+    final Builder builder = builderFactory.forMessage(messageType);
 
     InputStreamReader input = new InputStreamReader(entityStream, "UTF-8");
     if(isWrapped(type, genericType, annotations, mediaType)) {
@@ -102,6 +87,46 @@ public class ProtobufJsonReaderProvider implements MessageBodyReader<Object> {
     } else {
       return (Class<Message>) type;
     }
+  }
+
+  private final class BuilderFactory {
+
+    private Map<Class<Message>, Method> methodCache = new HashMap<Class<Message>, Method>();
+
+    Builder forMessage(final Class<Message> messageType) {
+      if(messageType == null) throw new IllegalArgumentException("messageType cannot be null");
+
+      try {
+        return (Builder) extractMethod(messageType).invoke(null);
+      } catch(WebApplicationException e) {
+        throw e;
+      } catch(RuntimeException e) {
+        log.error("Error invoking 'newBuilder' method for type " + messageType.getName(), e);
+        throw new WebApplicationException(500);
+      } catch(IllegalAccessException e) {
+        log.error("Error invoking 'newBuilder' method for type " + messageType.getName(), e);
+        throw new WebApplicationException(500);
+      } catch(InvocationTargetException e) {
+        log.error("Error invoking 'newBuilder' method for type " + messageType.getName(), e);
+        throw new WebApplicationException(500);
+      }
+    }
+
+    synchronized private Method extractMethod(final Class<Message> messageType) {
+
+      if(methodCache.containsKey(messageType) == false) {
+        try {
+          methodCache.put(messageType, messageType.getMethod("newBuilder"));
+        } catch(SecurityException e) {
+          log.error("Error getting 'newBuilder' method from type " + messageType.getName(), e);
+          throw new WebApplicationException(500);
+        } catch(NoSuchMethodException e) {
+          throw new IllegalStateException("The Message type " + messageType.getName() + " does not define a 'newBuilder' static method.");
+        }
+      }
+      return methodCache.get(messageType);
+    }
+
   }
 
 }
