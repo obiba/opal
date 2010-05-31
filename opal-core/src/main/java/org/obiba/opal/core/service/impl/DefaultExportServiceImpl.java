028a4627d444454edecffed2c95dea060059d8eb
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-05-31 15:23:39 +0000

    OPAL-350 * Removed transaction isolation setting.

--- opal-core/src/main/java/org/obiba/opal/core/service/impl/DefaultExportServiceImpl.java
+++ opal-core/src/main/java/org/obiba/opal/core/service/impl/DefaultExportServiceImpl.java
@@ -39,10 +39,8 @@ import org.obiba.opal.core.unit.FunctionalUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.util.Assert;
@@ -243,9 +241,7 @@ public class DefaultExportServiceImpl implements ExportService {
     }
 
     public void run() {
-      DefaultTransactionDefinition txdef = new DefaultTransactionDefinition();
-      txdef.setIsolationLevel(TransactionDefinition.ISOLATION_READ_UNCOMMITTED);
-      new TransactionTemplate(txManager, txdef).execute(new TransactionCallbackWithoutResult() {
+      new TransactionTemplate(txManager).execute(new TransactionCallbackWithoutResult() {
         @Override
         protected void doInTransactionWithoutResult(TransactionStatus status) {
           runnable.run();
