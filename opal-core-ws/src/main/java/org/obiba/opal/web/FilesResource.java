75819bf0b54e3d5ccbdc1b912412c95aa53b6777
Martin Boulanger <syrinx2004@gmail.com>
2010-06-04 19:19:28 +0000

    OPAL-393: Implement a service to upload a file to the Opal file system.

--- opal-core-ws/src/main/java/org/obiba/opal/web/FilesResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/FilesResource.java
@@ -9,15 +9,33 @@
  ******************************************************************************/
 package org.obiba.opal.web;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileType;
+import org.eclipse.jetty.util.log.Log;
+import org.obiba.core.util.StreamUtil;
 import org.obiba.opal.core.runtime.OpalRuntime;
 import org.obiba.opal.web.model.Opal;
 import org.slf4j.Logger;
@@ -92,7 +110,7 @@ public class FilesResource {
   }
 
   private Response getFile(FileObject file) {
-    return Response.ok(opalRuntime.getFileSystem().getLocalFile(file)).build();
+    return Response.ok(opalRuntime.getFileSystem().getLocalFile(file), MediaType.APPLICATION_OCTET_STREAM_TYPE).build();
   }
 
   private Response getFolder(FileObject folder) throws FileSystemException {
@@ -132,5 +150,73 @@ public class FilesResource {
       // Add the current child to the parent FileDto (folder).
       parentFolderBuilder.addChildren(fileBuilder.build());
     }
+  }
+
+  @PUT
+  // The POST method is required here to be compatible with Html forms which do not support the PUT method.
+  @POST
+  @Path("/{path:.*}")
+  public Response uploadFile(@PathParam("path") String path, @Context HttpServletRequest request) throws FileSystemException, FileUploadException {
+
+    FileObject fileToWriteTo = opalRuntime.getFileSystem().getRoot().resolveFile(path);
+    FileObject folderOfFileToWriteTo = fileToWriteTo.getParent();
+
+    List<FileItem> uploadedFiles = getUploadedFiles(request, fileToWriteTo);
+
+    // A folder exist with the same name at the specified path.
+    if(uploadedFiles.size() > 1) {
+      Log.warn("Multiple files upload is not supported by this service");
+      return Response.status(Status.BAD_REQUEST).build();
+
+      // Multiple files were uploaded (not supported).
+    } else if(fileToWriteTo.exists() && fileToWriteTo.getType() == FileType.FOLDER) {
+      Log.warn("Could not upload the file, a folder exist with that name at the specified path: {}", path);
+      return Response.status(Status.BAD_REQUEST).build();
+
+      // The parent folder does not exist (the specification says that we should not create folders)
+    } else if(folderOfFileToWriteTo != null && !folderOfFileToWriteTo.exists()) {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+
+    for(FileItem uploadedFile : uploadedFiles) {
+      writeUploadedFileToFileSystem(uploadedFile, fileToWriteTo);
+      break;
+    }
+
+    return Response.ok().build();
+
+  }
+
+  @SuppressWarnings("unchecked")
+  private List<FileItem> getUploadedFiles(HttpServletRequest request, FileObject fileToWriteTo) throws FileUploadException {
+    FileItemFactory factory = new DiskFileItemFactory();
+    ServletFileUpload upload = new ServletFileUpload(factory);
+    List<FileItem> uploadedFiles = new ArrayList<FileItem>();
+    for(FileItem fileItem : (List<FileItem>) upload.parseRequest(request)) {
+
+      // Consider only uploaded files, ignore any other form fields.
+      if(!fileItem.isFormField()) {
+        writeUploadedFileToFileSystem(fileItem, fileToWriteTo);
+        uploadedFiles.add(fileItem);
+      }
+
+    }
+    return uploadedFiles;
+  }
+
+  private void writeUploadedFileToFileSystem(FileItem uploadedFile, FileObject fileToWriteTo) {
+    OutputStream localFileStream = null;
+    InputStream uploadedFileStream = null;
+    try {
+      localFileStream = fileToWriteTo.getContent().getOutputStream();
+      uploadedFileStream = uploadedFile.getInputStream();
+      StreamUtil.copy(uploadedFileStream, localFileStream);
+    } catch(IOException couldNotWriteUploadedFile) {
+      throw new RuntimeException("Could not write uploaded file to Opal file system", couldNotWriteUploadedFile);
+    } finally {
+      StreamUtil.silentSafeClose(localFileStream);
+      StreamUtil.silentSafeClose(uploadedFileStream);
+    }
+
   }
 }
