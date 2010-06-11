14525b2c17d30cb6ec254ff4e150f42ceac90fae
Tony DeBat <tony.debat@oicr.on.ca>
2010-06-11 21:27:21 +0000

    OPAL-384 - Datasource view shows all the tables that are part of a datasource OPAL-383 - Presenter retrieves all the tables that are part of the datasource

---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/presenter/DatasourcePresenter.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.gwt.app.client.presenter;
+
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.place.Place;
+import net.customware.gwt.presenter.client.place.PlaceRequest;
+import net.customware.gwt.presenter.client.widget.WidgetDisplay;
+import net.customware.gwt.presenter.client.widget.WidgetPresenter;
+
+import org.obiba.opal.web.gwt.app.client.event.NavigatorSelectionChangeEvent;
+import org.obiba.opal.web.gwt.rest.client.ResourceCallback;
+import org.obiba.opal.web.gwt.rest.client.ResourceRequestBuilderFactory;
+import org.obiba.opal.web.model.client.DatasourceDto;
+import org.obiba.opal.web.model.client.TableDto;
+
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.ui.Label;
+import com.google.inject.Inject;
+
+public class DatasourcePresenter extends WidgetPresenter<DatasourcePresenter.Display> {
+
+  public interface Display extends WidgetDisplay {
+
+    void renderRows(JsArray<TableDto> rows);
+
+    Label getDatasourceNameLabel();
+
+    Label getVariableCountLabel();
+
+  }
+
+  private DatasourceDto datasource;
+
+  @Inject
+  public DatasourcePresenter(Display display, EventBus eventBus) {
+    super(display, eventBus);
+  }
+
+  public void setDatasource(DatasourceDto datasource) {
+    this.datasource = datasource;
+  }
+
+  @Override
+  public Place getPlace() {
+    return null;
+  }
+
+  @Override
+  protected void onBind() {
+    super.registerHandler(eventBus.addHandler(NavigatorSelectionChangeEvent.getType(), new NavigatorSelectionChangeEvent.Handler() {
+      @Override
+      public void onNavigatorSelectionChanged(NavigatorSelectionChangeEvent event) {
+        event.getSelection().getUserObject();
+        if(event.getSelection().getParentItem() == null) {
+          datasource = (DatasourceDto) event.getSelection().getUserObject();
+          String datasourceName = datasource.getName();
+          getDisplay().getDatasourceNameLabel().setText(datasourceName);
+          updateTable(datasourceName);
+        }
+      }
+    }));
+
+  }
+
+  @Override
+  protected void onPlaceRequest(PlaceRequest request) {
+  }
+
+  @Override
+  protected void onUnbind() {
+  }
+
+  @Override
+  public void refreshDisplay() {
+  }
+
+  @Override
+  public void revealDisplay() {
+  }
+
+  private void updateTable(final String datasource) {
+    ResourceRequestBuilderFactory.<JsArray<TableDto>> newBuilder().forResource("/datasource/" + datasource + "/tables").get().withCallback(new ResourceCallback<JsArray<TableDto>>() {
+      @Override
+      public void onResource(Response response, JsArray<TableDto> resource) {
+        getDisplay().getVariableCountLabel().setText(Integer.toString(resource.length()));
+        getDisplay().renderRows(resource);
+      }
+
+    }).send();
+  }
+
+}
---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/view/DatasourceView.java
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.gwt.app.client.view;
+
+import org.obiba.opal.web.gwt.app.client.i18n.Translations;
+import org.obiba.opal.web.gwt.app.client.js.JsArrays;
+import org.obiba.opal.web.gwt.app.client.presenter.DatasourcePresenter;
+import org.obiba.opal.web.model.client.TableDto;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListView;
+import com.google.gwt.view.client.SelectionModel;
+import com.google.gwt.view.client.SingleSelectionModel;
+import com.google.gwt.view.client.ListView.Delegate;
+
+public class DatasourceView extends Composite implements DatasourcePresenter.Display {
+  @UiTemplate("DatasourceView.ui.xml")
+  interface DatasourceViewUiBinder extends UiBinder<Widget, DatasourceView> {
+  }
+
+  private static DatasourceViewUiBinder uiBinder = GWT.create(DatasourceViewUiBinder.class);
+
+  @UiField
+  Label datasourceName;
+
+  @UiField
+  Label variableCount;
+
+  @UiField
+  CellTable<TableDto> table;
+
+  SelectionModel<TableDto> selectionModel = new SingleSelectionModel<TableDto>();
+
+  SimplePager<TableDto> pager;
+
+  private Translations translations = GWT.create(Translations.class);
+
+  public DatasourceView() {
+    initWidget(uiBinder.createAndBindUi(this));
+    addTableColumns();
+  }
+
+  private void addTableColumns() {
+    table.addColumn(new TextColumn<TableDto>() {
+
+      @Override
+      public String getValue(TableDto object) {
+        return object.getName();
+      }
+    }, translations.nameLabel());
+
+    table.addColumn(new TextColumn<TableDto>() {
+
+      @Override
+      public String getValue(TableDto object) {
+        return object.getEntityType();
+      }
+    }, translations.entityTypeLabel());
+
+    table.addColumn(new TextColumn<TableDto>() {
+
+      @Override
+      public String getValue(TableDto object) {
+        return Integer.toString(object.getVariableCount());
+      }
+    }, translations.variablesLabel());
+
+    table.setSelectionEnabled(true);
+    table.setSelectionModel(selectionModel);
+    table.setPageSize(20);
+    pager = new SimplePager<TableDto>(table);
+    table.setPager(pager);
+    LayoutPanel p = ((LayoutPanel) table.getParent());
+    p.add(pager);
+    p.setWidgetTopHeight(pager, 4, Unit.EM, 2, Unit.EM);
+    p.setWidgetRightWidth(pager, 1, Unit.EM, 50, Unit.PCT);
+    DOM.removeElementAttribute(pager.getElement(), "style");
+    DOM.setStyleAttribute(pager.getElement(), "cssFloat", "right");
+  }
+
+  @Override
+  public Widget asWidget() {
+    return this;
+  }
+
+  @Override
+  public void startProcessing() {
+  }
+
+  @Override
+  public void stopProcessing() {
+  }
+
+  @Override
+  public void renderRows(final JsArray<TableDto> rows) {
+    table.setDelegate(new Delegate<TableDto>() {
+
+      @Override
+      public void onRangeChanged(ListView<TableDto> listView) {
+        int start = listView.getRange().getStart();
+        int length = listView.getRange().getLength();
+        listView.setData(start, length, JsArrays.toList(rows, start, length));
+      }
+
+    });
+    pager.firstPage();
+    table.setData(0, table.getPageSize(), JsArrays.toList(rows, 0, table.getPageSize()));
+    table.setDataSize(rows.length(), true);
+  }
+
+  @Override
+  public Label getDatasourceNameLabel() {
+    return datasourceName;
+  }
+
+  @Override
+  public Label getVariableCountLabel() {
+    return variableCount;
+  }
+
+}
