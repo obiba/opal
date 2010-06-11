3333f7d9c2ab8dd867a56675102ef8cb6ea8a122
Tony DeBat <tony.debat@oicr.on.ca>
2010-06-11 21:58:57 +0000

    OPAL-389 - The Navigator tree as a reusable widget

---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/presenter/NavigatorTreePresenter.java
@@ -0,0 +1,125 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.gwt.app.client.presenter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.place.Place;
+import net.customware.gwt.presenter.client.place.PlaceRequest;
+import net.customware.gwt.presenter.client.widget.WidgetDisplay;
+import net.customware.gwt.presenter.client.widget.WidgetPresenter;
+
+import org.obiba.opal.web.gwt.app.client.event.NavigatorSelectionChangeEvent;
+import org.obiba.opal.web.gwt.app.client.event.SessionCreatedEvent;
+import org.obiba.opal.web.gwt.app.client.event.SessionExpiredEvent;
+import org.obiba.opal.web.gwt.rest.client.ResourceCallback;
+import org.obiba.opal.web.gwt.rest.client.ResourceRequestBuilderFactory;
+import org.obiba.opal.web.model.client.DatasourceDto;
+
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsArrayString;
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.ui.TreeItem;
+import com.google.inject.Inject;
+
+/**
+ * Presenter for a Tree displaying Opal datasources and tables.
+ */
+public class NavigatorTreePresenter extends WidgetPresenter<NavigatorTreePresenter.Display> {
+
+  public interface Display extends WidgetDisplay {
+
+    HasSelectionHandlers<TreeItem> getTree();
+
+    void setItems(List<TreeItem> items);
+
+    void clear();
+
+  }
+
+  @Inject
+  public NavigatorTreePresenter(final Display display, final EventBus eventBus) {
+    super(display, eventBus);
+  }
+
+  @Override
+  public Place getPlace() {
+    return null;
+  }
+
+  @Override
+  protected void onBind() {
+    super.registerHandler(getDisplay().getTree().addSelectionHandler(new SelectionHandler<TreeItem>() {
+      @Override
+      public void onSelection(SelectionEvent<TreeItem> event) {
+        eventBus.fireEvent(new NavigatorSelectionChangeEvent(event.getSelectedItem()));
+      }
+    }));
+
+    super.registerHandler(eventBus.addHandler(SessionCreatedEvent.getType(), new SessionCreatedEvent.Handler() {
+      @Override
+      public void onSessionCreated(SessionCreatedEvent event) {
+        refreshDisplay();
+      }
+    }));
+
+    super.registerHandler(eventBus.addHandler(SessionExpiredEvent.getType(), new SessionExpiredEvent.Handler() {
+      @Override
+      public void onSessionExpired(SessionExpiredEvent event) {
+        getDisplay().clear();
+      }
+    }));
+  }
+
+  @Override
+  protected void onPlaceRequest(PlaceRequest request) {
+  }
+
+  @Override
+  protected void onUnbind() {
+  }
+
+  @Override
+  public void refreshDisplay() {
+    updateTree();
+  }
+
+  @Override
+  public void revealDisplay() {
+    updateTree();
+  }
+
+  private void updateTree() {
+    ResourceRequestBuilderFactory.<JsArray<DatasourceDto>> newBuilder().forResource("/datasources").get().withCallback(new ResourceCallback<JsArray<DatasourceDto>>() {
+      @Override
+      public void onResource(Response response, JsArray<DatasourceDto> datasources) {
+        ArrayList<TreeItem> items = new ArrayList<TreeItem>(datasources.length());
+        for(int i = 0; i < datasources.length(); i++) {
+          DatasourceDto ds = datasources.get(i);
+          TreeItem dsItem = new TreeItem(ds.getName());
+          dsItem.setUserObject(ds);
+          JsArrayString array = ds.getTableArray();
+          for(int j = 0; j < array.length(); j++) {
+            array.get(j);
+            dsItem.addItem(array.get(j));
+          }
+          items.add(dsItem);
+        }
+        getDisplay().setItems(items);
+      }
+    }).send();
+  }
+
+}
---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/view/NavigatorTreeView.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.gwt.app.client.view;
+
+import java.util.List;
+
+import org.obiba.opal.web.gwt.app.client.presenter.NavigatorTreePresenter;
+
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * View for a Tree displaying Opal datasources and tables.
+ */
+public class NavigatorTreeView implements NavigatorTreePresenter.Display {
+
+  Tree tree;
+
+  public NavigatorTreeView() {
+    tree = new Tree();
+    tree.setAnimationEnabled(true);
+  }
+
+  @Override
+  public void setItems(List<TreeItem> items) {
+    tree.clear();
+    for(TreeItem item : items) {
+      tree.addItem(item);
+    }
+  }
+
+  @Override
+  public HasSelectionHandlers<TreeItem> getTree() {
+    return tree;
+  }
+
+  @Override
+  public void clear() {
+    this.tree.clear();
+  }
+
+  @Override
+  public Widget asWidget() {
+    return tree;
+  }
+
+  @Override
+  public void startProcessing() {
+  }
+
+  @Override
+  public void stopProcessing() {
+  }
+
+}
