f79906bf0b490ce9fee5ea535490045b8e882070
Martin Boulanger <syrinx2004@gmail.com>
2010-06-16 15:22:28 +0000

    OPAL-400: Fixed a bug that would trigger a download (download dialog would show up) in Firefox with uploading certain types of files (ex: zip, pdf, ...)

--- opal-core-ws/src/main/java/org/obiba/opal/web/FilesResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/FilesResource.java
@@ -21,6 +21,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -145,6 +146,7 @@ public class FilesResource {
   @POST
   @Path("/{path:.*}")
   @Consumes("multipart/form-data")
+  @Produces("text/html")
   public Response uploadFile(@PathParam("path") String path, @Context UriInfo uriInfo, @Context HttpServletRequest request) throws FileSystemException, FileUploadException {
 
     FileObject fileToWriteTo = resolveFileInFileSystem(path);
@@ -168,7 +170,7 @@ public class FilesResource {
 
     log.info("The following file was uploaded to Opal file system : {}", path);
 
-    return Response.created(uriInfo.getAbsolutePath()).build();
+    return Response.created(uriInfo.getAbsolutePath()).entity(path + " created").build();
 
   }
 
