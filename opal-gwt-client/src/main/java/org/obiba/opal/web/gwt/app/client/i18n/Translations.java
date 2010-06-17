6d2da4fb14c5d265879196dedfa67c82a01a922e
Tony DeBat <tony.debat@oicr.on.ca>
2010-06-17 16:21:05 +0000

    - Java translations now use MD5 keys just like the uiBinder translations. This means that the same translation can be used by both Java and uiBinder.

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/i18n/Translations.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/i18n/Translations.java
@@ -12,86 +12,166 @@ package org.obiba.opal.web.gwt.app.client.i18n;
 import java.util.Map;
 
 import com.google.gwt.i18n.client.Constants;
+import com.google.gwt.i18n.client.LocalizableResource.Generate;
+import com.google.gwt.i18n.client.LocalizableResource.GenerateKeys;
 
 /**
  * Programmatically available localised text strings. This interface will be bound to localised properties files found
  * in the {@code com.google.gwt.i18n.client} package.
  */
+@GenerateKeys
+@Generate(format = "com.google.gwt.i18n.rebind.format.PropertiesFormat", locales = { "default" })
 public interface Translations extends Constants {
 
+  @Description("Error dialog title")
+  @DefaultStringValue("Errors")
   String errorDialogTitle();
 
+  @Description("Error dialog title when used to display warnings")
+  @DefaultStringValue("Warnings")
   String warningDialogTitle();
 
+  @Description("Error dialog title when used to display information")
+  @DefaultStringValue("Information")
   String infoDialogTitle();
 
+  @Description("Variable Explorer column title")
+  @DefaultStringValue("Name")
   String nameLabel();
 
+  @Description("Variable Explorer column title")
+  @DefaultStringValue("Value Type")
   String valueTypeLabel();
 
+  @Description("Variable Explorer column title")
+  @DefaultStringValue("Label")
   String labelLabel();
 
+  @Description("ID label")
+  @DefaultStringValue("ID")
   String idLabel();
 
+  @Description("Type label")
+  @DefaultStringValue("Type")
   String typeLabel();
 
+  @Description("User label")
+  @DefaultStringValue("User")
   String userLabel();
 
+  @Description("Start label")
+  @DefaultStringValue("Start")
   String startLabel();
 
+  @Description("End label")
+  @DefaultStringValue("End")
   String endLabel();
 
+  @Description("Status label")
+  @DefaultStringValue("Status")
   String statusLabel();
 
+  @Description("Status map")
+  @DefaultStringMapValue( { "NOT_STARTED", "Not Started", "IN_PROGRESS", "In Progress", "SUCCEEDED", "Succeeded", "FAILED", "Failed", "CANCEL_PENDING", "Cancel Pending", "CANCELED", "Cancelled" })
   Map<String, String> statusMap();
 
+  @Description("Actions label")
+  @DefaultStringValue("Actions")
   String actionsLabel();
 
-  String sizeLabel();
-
-  String lastModifiedLabel();
-
-  String dateLabel();
-
-  String messageLabel();
-
-  String jobLabel();
-
-  String jobsLabel();
-
+  @Description("Action map")
+  @DefaultStringMapValue( { "Cancel", "Cancel", "Delete", "Delete" })
   Map<String, String> actionMap();
 
+  @Description("Size label")
+  @DefaultStringValue("Size")
+  String sizeLabel();
+
+  @Description("Last modified label")
+  @DefaultStringValue("Last Modified")
+  String lastModifiedLabel();
+
+  @Description("Date label")
+  @DefaultStringValue("Date")
+  String dateLabel();
+
+  @Description("Message label")
+  @DefaultStringValue("Message")
+  String messageLabel();
+
+  @Description("Job label")
+  @DefaultStringValue("Job")
+  String jobLabel();
+
+  @Description("Jobs menu item")
+  @DefaultStringValue("Jobs")
+  String jobsLabel();
+
+  @Description("File system label")
+  @DefaultStringValue("File System")
   String fileSystemLabel();
 
+  @Description("Entity type label")
+  @DefaultStringValue("Entity Type")
   String entityTypeLabel();
 
+  @Description("Variables menu item")
+  @DefaultStringValue("Variables")
   String variablesLabel();
 
+  @Description("Unit label")
+  @DefaultStringValue("Unit")
   String unitLabel();
 
+  @Description("User message map")
+  @DefaultStringMapValue( { "jobCancelled", "Job cancelled.", "jobDeleted", "Job deleted.", "SetCommandStatus_NotFound", "Job could not be cancelled (not found).", "SetCommandStatus_BadRequest_IllegalStatus", "Job status cannot be set to the specified value.", "SetCommandStatus_BadRequest_NotCancellable", "Job has completed and has already been cancelled.", "DeleteCommand_NotFound", "Job could not be deleted (not found).", "DeleteCommand_BadRequest_NotDeletable", "Job is currently running and therefore cannot be deleted at this time." })
   Map<String, String> userMessageMap();
 
+  @Description("You must select a file message")
+  @DefaultStringValue("You must select a file.")
   String fileMustBeSelected();
 
+  @Description("Yes label")
+  @DefaultStringValue("Yes")
   String yesLabel();
 
+  @Description("No label")
+  @DefaultStringValue("No")
   String noLabel();
 
+  @Description("Missing label")
+  @DefaultStringValue("Missing")
   String missingLabel();
 
+  @Description("Categories label")
+  @DefaultStringValue("Categories")
   String categoriesLabel();
 
+  @Description("Attributes label")
+  @DefaultStringValue("Attributes")
   String attributesLabel();
 
+  @Description("Language label")
+  @DefaultStringValue("Language")
   String languageLabel();
 
+  @Description("Value label")
+  @DefaultStringValue("Value")
   String valueLabel();
 
+  @Description("Code label")
+  @DefaultStringValue("Code")
   String codeLabel();
 
+  @Description("Mime Type label")
+  @DefaultStringValue("Mime Type")
   String mimeTypeLabel();
 
+  @Description("Repeatable label")
+  @DefaultStringValue("Repeatable")
   String repeatableLabel();
 
+  @Description("Occurrence Group label")
+  @DefaultStringValue("Occurrence Group")
   String occurrenceGroupLabel();
 }
