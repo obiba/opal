47b5edbdb03a6d6bf8d99b8744f80fd492b99863
Yannick Marcon <yannick.marcon@obiba.org>
2010-07-01 12:29:06 +0000

    Fixed broken variable presenter test.

--- opal-gwt-client/src/test/java/org/obiba/opal/client/presenter/VariablePresenterTest.java
+++ opal-gwt-client/src/test/java/org/obiba/opal/client/presenter/VariablePresenterTest.java
@@ -9,19 +9,24 @@
  ******************************************************************************/
 package org.obiba.opal.client.presenter;
 
+import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import net.customware.gwt.presenter.client.EventBus;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 import org.obiba.opal.web.gwt.app.client.event.VariableSelectionChangeEvent;
 import org.obiba.opal.web.gwt.app.client.presenter.VariablePresenter;
 import org.obiba.opal.web.gwt.test.AbstractGwtTestSetup;
 
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.GwtEvent.Type;
 
 public class VariablePresenterTest extends AbstractGwtTestSetup {
 
@@ -38,13 +43,17 @@ public class VariablePresenterTest extends AbstractGwtTestSetup {
     variablePresenter = new VariablePresenter(displayMock, eventBusMock);
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   public void testThatEventHandlersAreAddedToUIComponents() throws Exception {
     HandlerRegistration handlerRegistrationMock = createMock(HandlerRegistration.class);
-    VariableSelectionChangeEvent.Handler variableSelectionChangeEventMock = createMock(VariableSelectionChangeEvent.Handler.class);
-    expect(eventBusMock.addHandler(VariableSelectionChangeEvent.getType(), variableSelectionChangeEventMock)).andReturn(handlerRegistrationMock).anyTimes();
+    expect(eventBusMock.addHandler((Type<VariableSelectionChangeEvent.Handler>) EasyMock.anyObject(), (VariableSelectionChangeEvent.Handler) EasyMock.anyObject())).andReturn(handlerRegistrationMock).once();
 
-    replay(displayMock);
+    HasClickHandlers hasClickHandlerMock = createMock(HasClickHandlers.class);
+    expect(displayMock.getParentIcon()).andReturn(hasClickHandlerMock);
+    expect(hasClickHandlerMock.addClickHandler((ClickHandler) anyObject())).andReturn(handlerRegistrationMock).atLeastOnce();
+
+    replay(displayMock, eventBusMock, hasClickHandlerMock);
     variablePresenter.bind();
 
     verify(displayMock);
