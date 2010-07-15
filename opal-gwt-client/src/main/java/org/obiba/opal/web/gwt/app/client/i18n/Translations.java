1fbc570045c5666fbe72b17fae4b51fbf46efed9
Martin Boulanger <syrinx2004@gmail.com>
2010-07-15 21:54:27 +0000

    OPAL-502: Added some missing localization for delete confirmation dialog.

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/i18n/Translations.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/i18n/Translations.java
@@ -212,11 +212,11 @@ public interface Translations extends Constants {
   String logLabel();
 
   @Description("Confirmation title map")
-  @DefaultStringMapValue( { "clearJobsList", "Clear Jobs List", "cancelJob", "Cancel Job", "replaceExistingFile", "Replace File" })
+  @DefaultStringMapValue( { "clearJobsList", "Clear Jobs List", "cancelJob", "Cancel Job", "replaceExistingFile", "Replace File", "deleteFile", "Delete File" })
   Map<String, String> confirmationTitleMap();
 
   @Description("Confirmation message map")
-  @DefaultStringMapValue( { "confirmClearJobsList", "All the completed jobs (succeeded, failed or cancelled) will be removed from the jobs list. Currently running jobs will be unaffected.<br /><br />Please confirm that you want to clear the jobs list.", "confirmCancelJob", "The job will be cancelled. Changes will be rolled back as much as possible: although cancelled, a job might be partially completed.<br /><br />Please confirm that you want cancel this job.", "confirmReplaceExistingFile", "The file that you are uploading already exist in the file system.<br /><br />Please confirm that you want to replace the existing file." })
+  @DefaultStringMapValue( { "confirmClearJobsList", "All the completed jobs (succeeded, failed or cancelled) will be removed from the jobs list. Currently running jobs will be unaffected.<br /><br />Please confirm that you want to clear the jobs list.", "confirmCancelJob", "The job will be cancelled. Changes will be rolled back as much as possible: although cancelled, a job might be partially completed.<br /><br />Please confirm that you want cancel this job.", "confirmReplaceExistingFile", "The file that you are uploading already exist in the file system.<br /><br />Please confirm that you want to replace the existing file.", "confirmDeleteFile", "The file will be removed from the file system.<br /><br />Please confirm that you want to delete this file." })
   Map<String, String> confirmationMessageMap();
 
   @Description("A name is required when creating a new folder")
