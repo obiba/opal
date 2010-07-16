e395945e615605d9c6f72c4bed3e5a2a6d7528e8
Yannick Marcon <yannick.marcon@obiba.org>
2010-07-16 21:03:52 +0000

    Copy command online help updated.

--- opal-shell/src/main/java/org/obiba/opal/shell/commands/CopyCommand.java
+++ opal-shell/src/main/java/org/obiba/opal/shell/commands/CopyCommand.java
@@ -40,9 +40,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.google.common.collect.ImmutableSet;
 
 /**
- * Provides ability to copy Magma tables to an existing datasource or an Excel file.
+ * Provides ability to copy Magma tables to an existing datasource or a file based datasource.
  */
-@CommandUsage(description = "Copy tables to an existing destination datasource or to a specified Excel file. The tables can be explicitly named and/or be the ones from a specified source datasource. The variables can be optionally processed: dispatched in another table and/or renamed.", syntax = "Syntax: copy [--unit UNIT] [--source NAME] (--destination NAME | --out FILE) [--multiplex SCRIPT] [--transform SCRIPT] [--non-incremental] [--no-values | --no-variables] [TABLE_NAME...]")
+@CommandUsage(description = "Copy tables to an existing destination datasource or to a specified file. The tables can be explicitly named and/or be the ones from a specified source datasource. The variables can be optionally processed: dispatched in another table and/or renamed.", syntax = "Syntax: copy [--unit UNIT] [--source NAME] (--destination NAME | --out FILE) [--multiplex SCRIPT] [--transform SCRIPT] [--non-incremental] [--no-values | --no-variables] [TABLE_NAME...]")
 public class CopyCommand extends AbstractOpalRuntimeDependentCommand<CopyCommandOptions> {
 
   @Autowired
--- opal-shell/src/main/java/org/obiba/opal/shell/commands/options/CopyCommandOptions.java
+++ opal-shell/src/main/java/org/obiba/opal/shell/commands/options/CopyCommandOptions.java
@@ -31,7 +31,7 @@ public interface CopyCommandOptions extends HelpOption {
 
   public boolean isDestination();
 
-  @Option(shortName = "o", description = "Copy to a file.")
+  @Option(shortName = "o", description = "Copy to file: CSV if file is a directory, Excel if file has suffix xls or xlsx, XML if file has suffix zip.")
   public String getOut();
 
   public boolean isOut();
