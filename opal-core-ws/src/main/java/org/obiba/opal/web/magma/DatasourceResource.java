42966ba4a8d4d25e843a9bd910eb726945cc8a15
Yannick Marcon <yannick.marcon@obiba.org>
2010-08-10 16:54:34 +0000

    OPAL-653 DELETE of a transient datasource added. A regular datasource cannot be removed (yet). Request for deleting a non existing datasource is ignored.

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/DatasourceResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/DatasourceResource.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.PreDestroy;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -23,6 +24,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import org.obiba.core.util.StreamUtil;
@@ -81,6 +83,22 @@ public class DatasourceResource {
     Datasource ds = getDatasource();
 
     return Dtos.asDto(ds).build();
+  }
+
+  @DELETE
+  public Response removeDatasource() {
+    ResponseBuilder response = null;
+    if(MagmaEngine.get().hasTransientDatasource(name)) {
+      MagmaEngine.get().removeTransientDatasource(name);
+      response = Response.ok();
+    } else if(MagmaEngine.get().hasDatasource(name)) {
+      response = Response.status(Status.BAD_REQUEST).entity(getErrorMessage(Status.BAD_REQUEST, "NotTransientDatasourceRemovalUnsupported"));
+    } else {
+      // returns silently
+      response = Response.ok();
+    }
+
+    return response.build();
   }
 
   @GET
