137ef8db5e3f8672ff697f6a835cb45804269563
Yannick Marcon <yannick.marcon@obiba.org>
2010-08-11 15:53:14 +0000

    OPAL-653 JdbcDatasourceFactory Dto with parser added.

---
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/support/JdbcDatasourceFactoryDtoParser.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.magma.support;
+
+import java.util.Properties;
+
+import javax.transaction.TransactionManager;
+
+import org.obiba.magma.DatasourceFactory;
+import org.obiba.magma.datasource.jdbc.JdbcDatasourceFactory;
+import org.obiba.magma.datasource.jdbc.JdbcDatasourceSettings;
+import org.obiba.magma.datasource.jdbc.JdbcValueTableSettings;
+import org.obiba.opal.web.model.Magma.DatasourceFactoryDto;
+import org.obiba.opal.web.model.Magma.JdbcDatasourceFactoryDto;
+import org.obiba.opal.web.model.Magma.JdbcDatasourceSettingsDto;
+import org.obiba.opal.web.model.Magma.JdbcValueTableSettingsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ *
+ */
+@Component
+public class JdbcDatasourceFactoryDtoParser extends AbstractDatasourceFactoryDtoParser {
+
+  @Autowired(required = false)
+  private TransactionManager transactionManager;
+
+  @Override
+  protected DatasourceFactory internalParse(DatasourceFactoryDto dto) {
+    JdbcDatasourceFactory factory = null;
+    if(dto.hasJdbc()) {
+      factory = new JdbcDatasourceFactory();
+      JdbcDatasourceFactoryDto jdbcDto = dto.getJdbc();
+      factory.setTransactionManager(transactionManager);
+      factory.setJdbcProperties(parseProperties(jdbcDto));
+      if(jdbcDto.hasSettings()) {
+        factory.setDatasourceSettings(parseSettings(jdbcDto.getSettings()));
+      }
+    }
+    return factory;
+  }
+
+  private Properties parseProperties(JdbcDatasourceFactoryDto dto) {
+    Properties properties = new Properties();
+    properties.put(JdbcDatasourceFactory.DRIVER_CLASS_NAME, dto.getDriver());
+    properties.put(JdbcDatasourceFactory.URL, dto.getUrl());
+    properties.put(JdbcDatasourceFactory.USERNAME, dto.getUsername());
+    properties.put(JdbcDatasourceFactory.PASSWORD, dto.getPassword());
+    return properties;
+  }
+
+  private JdbcDatasourceSettings parseSettings(JdbcDatasourceSettingsDto dto) {
+    JdbcDatasourceSettings settings = new JdbcDatasourceSettings();
+    if(dto.hasDefaultEntityType()) {
+      settings.setDefaultEntityType(dto.getDefaultEntityType());
+    }
+    settings.setUseMetadataTables(dto.getUseMetadataTables());
+    ImmutableSet<String> mappedTables = (new ImmutableSet.Builder<String>()).addAll(dto.getMappedTablesList()).build();
+    settings.setMappedTables(mappedTables);
+    ImmutableSet.Builder<JdbcValueTableSettings> tableSettings = new ImmutableSet.Builder<JdbcValueTableSettings>();
+    for(JdbcValueTableSettingsDto tableDto : dto.getTableSettingsList()) {
+      tableSettings.add(parseSettings(tableDto));
+    }
+    settings.setTableSettings(tableSettings.build());
+    if(dto.hasDefaultCreatedTimestampColumnName()) {
+      settings.setDefaultCreatedTimestampColumnName(dto.getDefaultCreatedTimestampColumnName());
+    }
+    if(dto.hasDefaultUpdatedTimestampColumnName()) {
+      settings.setDefaultUpdatedTimestampColumnName(dto.getDefaultUpdatedTimestampColumnName());
+    }
+    return settings;
+  }
+
+  private JdbcValueTableSettings parseSettings(JdbcValueTableSettingsDto dto) {
+    JdbcValueTableSettings settings = new JdbcValueTableSettings();
+    settings.setSqlTableName(dto.getSqlTableName());
+    if(dto.hasMagmaTableName()) {
+      settings.setMagmaTableName(dto.getMagmaTableName());
+    }
+    settings.setEntityType(dto.getEntityType());
+    settings.setEntityIdentifierColumns(dto.getEntityIdentifierColumnsList());
+    if(dto.hasCreatedTimestampColumnName()) {
+      settings.setCreatedTimestampColumnName(dto.getCreatedTimestampColumnName());
+    }
+    if(dto.hasUpdatedTimestampColumnName()) {
+      settings.setUpdatedTimestampColumnName(dto.getUpdatedTimestampColumnName());
+    }
+    return settings;
+  }
+
+  public void setTransactionManager(TransactionManager transactionManager) {
+    this.transactionManager = transactionManager;
+  }
+
+}
