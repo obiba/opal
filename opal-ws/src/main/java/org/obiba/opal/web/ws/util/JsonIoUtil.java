055b8ee0d850d6718f7edb66be04f38d5c49f9bb
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-08-12 15:45:34 +0000

    * Fixed PMD warning * Added unit tests for JsonIoUtil

--- opal-ws/src/main/java/org/obiba/opal/web/ws/util/JsonIoUtil.java
+++ opal-ws/src/main/java/org/obiba/opal/web/ws/util/JsonIoUtil.java
@@ -1,7 +1,7 @@
 package org.obiba.opal.web.ws.util;
 
 import java.io.IOException;
-import java.nio.CharBuffer;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -18,46 +18,41 @@ public final class JsonIoUtil {
 
   private static final char JS_ARRAY_OPEN = '[';
 
+  private static final String JS_ARRAY_OPEN_STR = String.valueOf(JS_ARRAY_OPEN);
+
   private static final char JS_ARRAY_SEP = ',';
 
+  private static final String JS_ARRAY_SEP_STR = String.valueOf(JS_ARRAY_SEP);
+
   private static final char JS_ARRAY_CLOSE = ']';
+
+  private static final String JS_ARRAY_CLOSE_STR = String.valueOf(JS_ARRAY_CLOSE);
 
   private JsonIoUtil() {
   }
 
-  public static <T extends Message> ArrayList<T> mergeCollection(final Readable readable, final Builder builder) throws IOException {
-    return mergeCollection(readable, ExtensionRegistry.getEmptyRegistry(), builder);
+  public static <T extends Message> ArrayList<T> mergeCollection(final Reader reader, final Builder builder) throws IOException {
+    return mergeCollection(reader, ExtensionRegistry.getEmptyRegistry(), builder);
   }
 
-  public static <T extends Message> ArrayList<T> mergeCollection(final Readable readable, final ExtensionRegistry extensionRegistry, final Builder builder) throws IOException {
-    if(builder == null) throw new IllegalArgumentException("builder cannot be null");
+  public static <T extends Message> ArrayList<T> mergeCollection(final Reader reader, final ExtensionRegistry extensionRegistry, final Builder builder) throws IOException {
+    if(reader == null) throw new IllegalArgumentException("reader cannot be null");
     if(extensionRegistry == null) throw new IllegalArgumentException("extensionRegistry cannot be null");
-    if(readable == null) throw new IllegalArgumentException("readable cannot be null");
+    if(builder == null) throw new IllegalArgumentException("builder cannot be null");
 
-    CharBuffer cb = CharBuffer.allocate(1);
-    // Read the array opening tag '['
-    int read = readable.read(cb);
-    if(read != 1 && cb.charAt(0) != JS_ARRAY_OPEN) {
-      throw new IOException("unexpected state");
-    }
-    ArrayList<T> messages = new ArrayList<T>();
-    do {
-      builder.clear();
-      JsonFormat.merge(readable, builder);
-      messages.add((T) builder.build());
-      // read the next char which is either ',' or ']'
-      read = readable.read(cb);
-    } while(read > 0 && cb.charAt(0) == JS_ARRAY_SEP);
+    final ArrayList<T> messages = new ArrayList<T>();
+    InnerJsonFormat.mergeCollection(reader, extensionRegistry, builder, new MergeCallback() {
 
-    // Make sure we read the closing character, and not some other strange char
-    if(cb.charAt(0) != JS_ARRAY_CLOSE) {
-      throw new IOException("unexpected state");
-    }
+      @Override
+      @SuppressWarnings("unchecked")
+      public void onMerge(Builder builder) {
+        messages.add((T) builder.build());
+      }
+    });
     return messages;
-
   }
 
-  public static void printCollection(final Collection<Message> messages, final Appendable appendable) throws IOException {
+  public static void printCollection(final Collection<? extends Message> messages, final Appendable appendable) throws IOException {
     if(messages == null) throw new IllegalArgumentException("messages cannot be null");
     if(appendable == null) throw new IllegalArgumentException("messages cannot be null");
 
@@ -74,4 +69,47 @@ public final class JsonIoUtil {
     // Close the Array
     appendable.append(']');
   }
+
+  /**
+   * Callback called when a Message instance part of a collection has been read. Ususally, the message instance would be
+   * added to a collection.
+   */
+  private static interface MergeCallback {
+
+    /**
+     * Called when a Message has been read from a stream of Messages. Note that this method may never be called if the
+     * stream contains 0 Messages.
+     * @param builder
+     */
+    public void onMerge(Builder builder);
+  }
+
+  /**
+   * Used to call protected methods on JsonFormat
+   */
+  private static final class InnerJsonFormat extends JsonFormat {
+
+    static void mergeCollection(final Reader reader, final ExtensionRegistry extensionRegistry, final Builder builder, final MergeCallback callback) throws IOException {
+      CharSequence input = toStringBuilder(reader);
+      Tokenizer tokenizer = new Tokenizer(input.subSequence(0, input.length()));
+      tokenizer.consume(JS_ARRAY_OPEN_STR);
+      // Special case of empty list of messages: "[]"
+      if(tokenizer.tryConsume(JS_ARRAY_CLOSE_STR) == false) {
+        // At least one Message present
+        do {
+          Builder thisBuilder = builder.clone();
+          tokenizer.consume("{"); // Needs to happen when the object starts.
+          while(!tokenizer.tryConsume("}")) { // Continue till the object is done
+            mergeField(tokenizer, extensionRegistry, thisBuilder);
+          }
+          callback.onMerge(thisBuilder);
+          // Iterate if we consume a separator.
+        } while(tokenizer.tryConsume(JS_ARRAY_SEP_STR));
+
+        // Make sure the next character terminates the array
+        tokenizer.consume(JS_ARRAY_CLOSE_STR);
+      }
+    }
+  }
+
 }
---
+++ opal-ws/src/test/java/org/obiba/opal/web/ws/util/JsonIoUtilTest.java
@@ -0,0 +1,136 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.ws.util;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.obiba.opal.web.model.Magma.DatasourceDto;
+
+/**
+ *
+ */
+public class JsonIoUtilTest {
+
+  @Test
+  public void test_printCollectionWithEmptyCollection() throws IOException {
+    StringBuilder output = new StringBuilder();
+    JsonIoUtil.printCollection(createDtos(), output);
+    assertThat(output.toString(), is("[]"));
+  }
+
+  @Test
+  public void test_printCollectionWithSingletonCollection() throws IOException {
+    StringBuilder output = new StringBuilder();
+    JsonIoUtil.printCollection(createDtos("singleton"), output);
+    assertThat(output.toString(), is(createJs("singleton")));
+  }
+
+  @Test
+  public void test_printCollectionWithTwoElements() throws IOException {
+    StringBuilder output = new StringBuilder();
+    JsonIoUtil.printCollection(createDtos("first", "second"), output);
+    assertThat(output.toString(), is(createJs("first", "second")));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_printCollection_cannotPassNullCollection() throws IOException {
+    JsonIoUtil.printCollection(null, new StringBuilder());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_printCollection_cannotPassNullApendable() throws IOException {
+    JsonIoUtil.printCollection(createDtos(), null);
+  }
+
+  @Test
+  public void test_mergeCollectionWithEmptyArray() throws IOException {
+    Collection<DatasourceDto> msgs = JsonIoUtil.mergeCollection(new StringReader("[]"), DatasourceDto.newBuilder());
+    assertThat(msgs, notNullValue());
+    assertThat(msgs.size(), is(0));
+  }
+
+  @Test
+  public void test_mergeCollectionSingleElement() throws IOException {
+    Collection<DatasourceDto> msgs = JsonIoUtil.mergeCollection(new StringReader(createJs("singleton")), DatasourceDto.newBuilder());
+    assertThat(msgs, notNullValue());
+    assertThat(msgs, is(createDtos("singleton")));
+  }
+
+  @Test
+  public void test_mergeCollectionMultipleElements() throws IOException {
+    Collection<DatasourceDto> msgs = JsonIoUtil.mergeCollection(new StringReader(createJs("first", "second")), DatasourceDto.newBuilder());
+    assertThat(msgs, notNullValue());
+    assertThat(msgs, is(createDtos("first", "second")));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_mergeCollection_cannotPassNullReader() throws IOException {
+    JsonIoUtil.mergeCollection(null, DatasourceDto.newBuilder());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_mergeCollection_cannotPassNullBuilder() throws IOException {
+    JsonIoUtil.mergeCollection(new StringReader(""), null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_mergeCollection_cannotPassNullExtensionRegistry() throws IOException {
+    JsonIoUtil.mergeCollection(new StringReader(""), null, DatasourceDto.newBuilder());
+  }
+
+  private Collection<DatasourceDto> createDtos(String... names) {
+    ArrayList<DatasourceDto> dtos = new ArrayList<DatasourceDto>(names.length);
+    for(String name : names) {
+      dtos.add(DatasourceDto.newBuilder().setName(name).setLink("http://localhost/" + name).build());
+    }
+    return dtos;
+  }
+
+  /**
+   * Creates the expected JSON string for a list of names. Each element in the {@code names} array will produce this:
+   * 
+   * <pre>
+   * {"name": "names[i]","link": "http://localhost/names[i]"}
+   * </pre>
+   * 
+   * They are all wrapped in a JSON array:
+   * 
+   * <pre>
+   * [{...},{...},...]
+   * </pre>
+   * 
+   * An empty array produces an empty JSON array:
+   * 
+   * <pre>
+   * []
+   * </pre>
+   * 
+   * @param names
+   * @return
+   */
+  private static String createJs(String... names) {
+    StringBuilder sb = new StringBuilder();
+    sb.append('[');
+    for(int i = 0; i < names.length; i++) {
+      if(i > 0) sb.append(',');
+      sb.append("{\"name\": \"").append(names[i]).append("\",\"link\": \"").append("http://localhost/").append(names[i]).append("\"}");
+    }
+    sb.append(']');
+    return sb.toString();
+  }
+}
