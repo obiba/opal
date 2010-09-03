9cce29c4903fc1d846bd3ee55746a16e7d949652
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-09-03 00:59:34 +0000

    * Moved hsqldb from runtime scope to test scope * Added hsqldb to opal-server runtime dependencies * Removed protostuff from runtime dependencies * Created OpalJavaClient for interfacing opal web services with Java.

---
+++ opal-rest-client/src/main/java/org/obiba/opal/rest/client/magma/OpalJavaClient.java
@@ -0,0 +1,120 @@
+package org.obiba.opal.rest.client.magma;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpMessage;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.obiba.core.util.StreamUtil;
+import org.obiba.magma.MagmaRuntimeException;
+
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.Message;
+
+/**
+ * A Java client for Opal RESTful services.
+ */
+public class OpalJavaClient {
+
+  private final URI opalURI;
+
+  private final HttpClient client;
+
+  private final BasicHttpContext ctx;
+
+  private final BasicCookieStore cs;
+
+  public OpalJavaClient(String uri, String username, String password) throws URISyntaxException {
+    if(uri == null) throw new IllegalArgumentException("uri cannot be null");
+    if(username == null) throw new IllegalArgumentException("username cannot be null");
+    if(password == null) throw new IllegalArgumentException("password cannot be null");
+
+    this.opalURI = new URI(uri.endsWith("/") ? uri : uri + "/");
+
+    DefaultHttpClient httpClient = new DefaultHttpClient();
+    httpClient.getCredentialsProvider().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, new String(password)));
+    httpClient.getParams().setParameter("http.protocol.handle-authentication", Boolean.TRUE);
+    httpClient.getParams().setParameter("http.auth.target-scheme-pref", ImmutableList.of(OpalAuthScheme.NAME));
+    httpClient.getAuthSchemes().register(OpalAuthScheme.NAME, new OpalAuthScheme.Factory());
+    ctx = new BasicHttpContext();
+    ctx.setAttribute(ClientContext.AUTH_SCHEME_PREF, ImmutableList.of(OpalAuthScheme.NAME));
+    ctx.setAttribute(ClientContext.COOKIE_STORE, cs = new BasicCookieStore());
+    this.client = httpClient;
+  }
+
+  public URI buildURI(String... segments) {
+    return buildURI(this.opalURI, segments);
+  }
+
+  public URI buildURI(final URI root, String... segments) {
+    URI uri = root;
+    for(String segment : segments) {
+      segment = segment.endsWith("/") ? segment : segment + "/";
+      uri = uri.resolve(segment);
+    }
+    return uri;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T extends Message> List<T> getResources(URI uri, Message.Builder builder) {
+    ArrayList<T> resources = new ArrayList<T>();
+    InputStream is = null;
+    try {
+      HttpResponse response = get(uri);
+      is = response.getEntity().getContent();
+      while(builder.mergeDelimitedFrom(is)) {
+        T message = (T) builder.build();
+        resources.add(message);
+        builder = message.newBuilderForType();
+      }
+      return resources;
+    } catch(IOException e) {
+      throw new MagmaRuntimeException(e);
+    } finally {
+      StreamUtil.silentSafeClose(is);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T extends Message> T getResource(URI uri, Message.Builder builder) {
+    InputStream is = null;
+    try {
+      HttpResponse response = get(uri);
+      is = response.getEntity().getContent();
+      return (T) builder.mergeFrom(is).build();
+    } catch(IOException e) {
+      throw new MagmaRuntimeException(e);
+    } finally {
+      StreamUtil.silentSafeClose(is);
+    }
+  }
+
+  public HttpResponse get(URI uri) throws ClientProtocolException, IOException {
+    HttpGet get = new HttpGet(uri);
+    get.addHeader("Accept", "application/x-protobuf");
+    authenticate(get);
+    return client.execute(get, ctx);
+  }
+
+  private void authenticate(HttpMessage msg) {
+    for(Cookie c : cs.getCookies()) {
+      if(c.getName().equalsIgnoreCase("opalsid")) {
+        msg.addHeader(OpalAuthScheme.NAME, c.getValue());
+      }
+    }
+  }
+}
--- opal-rest-client/src/main/java/org/obiba/opal/rest/client/magma/RestDatasource.java
+++ opal-rest-client/src/main/java/org/obiba/opal/rest/client/magma/RestDatasource.java
@@ -1,34 +1,16 @@
 package org.obiba.opal.rest.client.magma;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.http.HttpMessage;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.ClientContext;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.obiba.core.util.StreamUtil;
-import org.obiba.magma.MagmaRuntimeException;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.support.AbstractDatasource;
 import org.obiba.magma.support.Initialisables;
 import org.obiba.opal.web.model.Magma.TableDto;
 
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
@@ -37,30 +19,20 @@ import com.google.protobuf.Message;
 
 public class RestDatasource extends AbstractDatasource {
 
+  private final OpalJavaClient opalClient;
+
   private final URI datasourceURI;
-
-  private final HttpClient client;
-
-  private final BasicHttpContext ctx;
-
-  private final BasicCookieStore cs;
 
   private Set<String> cachedTableNames;
 
-  public RestDatasource(String name, String uri, String username, char[] password) throws URISyntaxException {
+  public RestDatasource(String name, String opalUri, String remoteDatasource, String username, String password) throws URISyntaxException {
+    this(name, new OpalJavaClient(opalUri, username, password), remoteDatasource);
+  }
+
+  public RestDatasource(String name, OpalJavaClient opalClient, String remoteDatasource) {
     super(name, "rest");
-
-    DefaultHttpClient httpClient = new DefaultHttpClient();
-    httpClient.getCredentialsProvider().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, new String(password)));
-    httpClient.getParams().setParameter("http.protocol.handle-authentication", Boolean.TRUE);
-    httpClient.getParams().setParameter("http.auth.target-scheme-pref", ImmutableList.of(OpalAuthScheme.NAME));
-    httpClient.getAuthSchemes().register(OpalAuthScheme.NAME, new OpalAuthScheme.Factory());
-    ctx = new BasicHttpContext();
-    ctx.setAttribute(ClientContext.AUTH_SCHEME_PREF, ImmutableList.of(OpalAuthScheme.NAME));
-    ctx.setAttribute(ClientContext.COOKIE_STORE, cs = new BasicCookieStore());
-
-    this.client = httpClient;
-    this.datasourceURI = new URI(uri.endsWith("/") ? uri : uri + "/");
+    this.opalClient = opalClient;
+    this.datasourceURI = opalClient.buildURI(remoteDatasource);
   }
 
   @Override
@@ -79,7 +51,7 @@ public class RestDatasource extends AbstractDatasource {
   protected void onDispose() {
     super.onDispose();
     try {
-      client.getConnectionManager().shutdown();
+      //      client.getConnectionManager().shutdown();
     } catch(Exception ignore) {
       // ignore
     }
@@ -121,64 +93,18 @@ public class RestDatasource extends AbstractDatasource {
   }
 
   URI newReference(String... segments) {
-    return buildURI(this.datasourceURI, segments);
+    return opalClient.buildURI(this.datasourceURI, segments);
   }
 
   URI buildURI(final URI root, String... segments) {
-    URI uri = root;
-    for(String segment : segments) {
-      segment = segment.endsWith("/") ? segment : segment + "/";
-      uri = uri.resolve(segment);
-    }
-    return uri;
+    return opalClient.buildURI(root, segments);
   }
 
-  @SuppressWarnings("unchecked")
   <T extends Message> List<T> readResources(URI uri, Message.Builder builder) {
-    ArrayList<T> resources = new ArrayList<T>();
-    InputStream is = null;
-    try {
-      HttpResponse response = get(uri);
-      is = response.getEntity().getContent();
-      while(builder.mergeDelimitedFrom(is)) {
-        T message = (T) builder.build();
-        resources.add(message);
-        builder = message.newBuilderForType();
-      }
-      return resources;
-    } catch(IOException e) {
-      throw new MagmaRuntimeException(e);
-    } finally {
-      StreamUtil.silentSafeClose(is);
-    }
+    return opalClient.getResources(uri, builder);
   }
 
-  @SuppressWarnings("unchecked")
   <T extends Message> T readResource(URI uri, Message.Builder builder) {
-    InputStream is = null;
-    try {
-      HttpResponse response = get(uri);
-      is = response.getEntity().getContent();
-      return (T) builder.mergeFrom(is).build();
-    } catch(IOException e) {
-      throw new MagmaRuntimeException(e);
-    } finally {
-      StreamUtil.silentSafeClose(is);
-    }
-  }
-
-  HttpResponse get(URI uri) throws ClientProtocolException, IOException {
-    HttpGet get = new HttpGet(uri);
-    get.addHeader("Accept", "application/x-protobuf");
-    authenticate(get);
-    return client.execute(get, ctx);
-  }
-
-  void authenticate(HttpMessage msg) {
-    for(Cookie c : cs.getCookies()) {
-      if(c.getName().equalsIgnoreCase("opalsid")) {
-        msg.addHeader(OpalAuthScheme.NAME, c.getValue());
-      }
-    }
+    return opalClient.getResource(uri, builder);
   }
 }
