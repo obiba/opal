800126df176a5388620beefcc76971e95a43ab53
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-09-24 01:51:17 +0000

    OPAL-745:   - Implemented web service for getting a block of values for a variable -- regular table.

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/VariableResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/VariableResource.java
@@ -9,25 +9,28 @@
  ******************************************************************************/
 package org.obiba.opal.web.magma;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.TreeSet;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 import org.obiba.magma.Value;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.VariableEntity;
 import org.obiba.magma.VariableValueSource;
 import org.obiba.magma.VectorSource;
+import org.obiba.opal.web.magma.support.InvalidRequestException;
 import org.obiba.opal.web.math.AbstractSummaryStatisticsResource;
 import org.obiba.opal.web.math.CategoricalSummaryStatisticsResource;
 import org.obiba.opal.web.math.ContinuousSummaryStatisticsResource;
 import org.obiba.opal.web.math.DefaultSummaryStatisticsResource;
 import org.obiba.opal.web.model.Magma.ValueDto;
 import org.obiba.opal.web.model.Magma.VariableDto;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 
 public class VariableResource {
 
@@ -45,15 +48,27 @@ public class VariableResource {
     return Dtos.asDto(vvs.getVariable()).build();
   }
 
+  @GET
   @Path("/values")
-  public Iterable<ValueDto> getValues() {
+  public Iterable<ValueDto> getValues(@QueryParam("offset") Integer offset, @QueryParam("limit") Integer limit) {
+    if(limit == null) {
+      throw new InvalidRequestException("'limit' parameter required");
+    }
+    if(limit < 0) {
+      throw new InvalidRequestException("Illegal 'limit' parameter value (" + limit + ")");
+    }
+
     VectorSource vectorSource = vvs.asVectorSource();
 
     if(vectorSource != null) {
-      Iterable<Value> values = vectorSource.getValues(new TreeSet<VariableEntity>(valueTable.getVariableEntities()));
-      return Iterables.transform(values, Dtos.valueAsDtoFunc);
+      Iterable<Value> values = subList(vectorSource.getValues(new TreeSet<VariableEntity>(valueTable.getVariableEntities())), offset != null ? offset : 0, limit);
+      List<ValueDto> valueDtos = new ArrayList<ValueDto>();
+      for(Value value : values) {
+        valueDtos.add(Dtos.asDto(value).build());
+      }
+      return valueDtos;
     }
-    return ImmutableList.of();
+    return Collections.emptyList();
   }
 
   @Path("/summary")
@@ -70,4 +85,20 @@ public class VariableResource {
     return new DefaultSummaryStatisticsResource(this.valueTable, this.vvs.getVariable(), this.vvs.asVectorSource());
   }
 
+  private <T> Iterable<T> subList(Iterable<T> iterable, int offset, int limit) {
+    List<T> subList = new ArrayList<T>();
+
+    Iterator<T> iterator = iterable.iterator();
+    for(int i = 0; i < offset; i++) {
+      iterator.next();
+    }
+
+    int count = 0;
+    while(iterator.hasNext() && count < limit) {
+      subList.add(iterator.next());
+      count++;
+    }
+
+    return subList;
+  }
 }
