65c5620caf0a6391f9ec8dd23958bb654b914428
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-10-22 20:27:04 +0000

    OPAL-816:   - Reverted changes due to a cyclic dependency issue.

--- opal-shell/src/main/java/org/obiba/opal/shell/commands/ReportCommand.java
+++ opal-shell/src/main/java/org/obiba/opal/shell/commands/ReportCommand.java
@@ -9,70 +9,20 @@
  ******************************************************************************/
 package org.obiba.opal.shell.commands;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.obiba.opal.core.cfg.ReportTemplate;
-import org.obiba.opal.reporting.service.ReportException;
-import org.obiba.opal.reporting.service.ReportService;
 import org.obiba.opal.shell.commands.options.ReportCommandOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @CommandUsage(description = "Generate a report based on the specified report template.", syntax = "Syntax: report --name TEMPLATE")
 public class ReportCommand extends AbstractOpalRuntimeDependentCommand<ReportCommandOptions> {
-  //
-  // Constants
-  //
-
-  private static final String DATE_FORMAT_PATTERN = "yyyyMMdd_HHmm";
-
-  //
-  // Instance Variables
-  //
-
-  @Autowired
-  private ReportService reportService;
-
-  //
-  // AbstractOpalRuntimeDependentCommand Methods
-  //
 
   @Override
   public int execute() {
-    // Get the report template.
-    String reportTemplateName = getOptions().getName();
-    ReportTemplate reportTemplate = this.getOpalRuntime().getOpalConfiguration().getReportTemplate(reportTemplateName);
-    if(reportTemplate == null) {
-      getShell().printf("Report template '%s' does not exist.\n", reportTemplateName);
-      return 1;
-    }
-
-    // Render it.
-    try {
-      reportService.render(reportTemplate.getFormat(), reportTemplate.getParameters(), reportTemplate.getDesign(), getReportOutput(reportTemplateName, reportTemplate.getFormat()));
-    } catch(ReportException ex) {
-      getShell().printf("Error rendering report: '%s'\n", ex.getMessage());
-      return 2;
-    }
-
-    // TODO: Send notification email.
-
+    // TODO implement ReportCommand.
+    System.out.println("Executing report command!");
     return 0;
   }
 
-  //
-  // Methods
-  //
-
-  @Override
   public String toString() {
     return "report -n " + getOptions().getName();
   }
 
-  private String getReportOutput(String reportTemplateName, String reportFormat) {
-    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_PATTERN);
-    String reportDate = dateFormat.format(new Date());
-
-    return reportTemplateName + "-" + reportDate + "." + reportFormat;
-  }
 }
