e37d404a93575538650f4e05291b5619657d408d
Tony DeBat <tony.debat@oicr.on.ca>
2010-11-09 23:43:14 +0000

    OPAL-762 Added a VariableAddRequiredEvent to be sent when the user has request a new variable be added.

---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/configureview/event/VariableAddRequiredEvent.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.obiba.opal.web.gwt.app.client.wizard.configureview.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * Signals that new variable should be added to the form. The new variable name is supplied.
+ */
+public class VariableAddRequiredEvent extends GwtEvent<VariableAddRequiredEvent.Handler> {
+
+  private static Type<Handler> TYPE;
+
+  private String variableName;
+
+  public VariableAddRequiredEvent(String variableName) {
+    this.variableName = variableName;
+  }
+
+  @Override
+  protected void dispatch(Handler handler) {
+    handler.onVariableAddRequired(this);
+  }
+
+  @Override
+  public Type<Handler> getAssociatedType() {
+    return TYPE;
+  }
+
+  public static Type<Handler> getType() {
+    return TYPE != null ? TYPE : (TYPE = new Type<Handler>());
+  }
+
+  public String getVariableName() {
+    return variableName;
+  }
+
+  public interface Handler extends EventHandler {
+    public void onVariableAddRequired(VariableAddRequiredEvent event);
+  }
+
+}
