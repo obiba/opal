24e8be43886fad55a53988e9646788b711f55ebf
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-12-03 05:05:42 +0000

    OPAL-943:   - Fixed PMD warning in WizardManager (onWizardRequired method too long).   - Also added an unbind() method to WizardManager, for consistency.

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/WizardManager.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/WizardManager.java
@@ -9,6 +9,9 @@
  ******************************************************************************/
 package org.obiba.opal.web.gwt.app.client.wizard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.customware.gwt.presenter.client.EventBus;
 
 import org.obiba.opal.web.gwt.app.client.wizard.createdatasource.presenter.CreateDatasourcePresenter;
@@ -18,6 +21,7 @@ import org.obiba.opal.web.gwt.app.client.wizard.exportdata.presenter.DataExportP
 import org.obiba.opal.web.gwt.app.client.wizard.importdata.presenter.DataImportPresenter;
 import org.obiba.opal.web.gwt.app.client.wizard.importvariables.presenter.VariablesImportPresenter;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
@@ -32,21 +36,25 @@ public class WizardManager {
   private final EventBus eventBus;
 
   @Inject
-  private Provider<CreateDatasourcePresenter> createDatasourceWizard;
+  private Provider<CreateDatasourcePresenter> createDatasourceWizardProvider;
 
   @Inject
-  private Provider<CreateViewStepPresenter> createViewWizard;
+  private Provider<CreateViewStepPresenter> createViewWizardProvider;
 
   @Inject
-  private Provider<VariablesImportPresenter> variablesImportWizard;
+  private Provider<VariablesImportPresenter> variablesImportWizardProvider;
 
   @Inject
-  private Provider<DataImportPresenter> dataImportWizard;
+  private Provider<DataImportPresenter> dataImportWizardProvider;
 
   @Inject
-  private Provider<DataExportPresenter> dataExportWizard;
+  private Provider<DataExportPresenter> dataExportWizardProvider;
+
+  private Map<WizardType, Provider<? extends Wizard>> wizardProviders;
 
   private Wizard lastBoundWizard;
+
+  private HandlerRegistration handlerRegistration;
 
   //
   // Constructors
@@ -62,7 +70,22 @@ public class WizardManager {
   //
 
   public void bind() {
-    eventBus.addHandler(WizardRequiredEvent.getType(), new WizardRequiredEventHandler());
+    wizardProviders = new HashMap<WizardType, Provider<? extends Wizard>>();
+    wizardProviders.put(WizardType.CREATE_DATASOURCE, createDatasourceWizardProvider);
+    wizardProviders.put(WizardType.CREATE_VIEW, createViewWizardProvider);
+    wizardProviders.put(WizardType.IMPORT_VARIABLES, variablesImportWizardProvider);
+    wizardProviders.put(WizardType.IMPORT_DATA, dataImportWizardProvider);
+    wizardProviders.put(WizardType.EXPORT_DATA, dataExportWizardProvider);
+
+    handlerRegistration = eventBus.addHandler(WizardRequiredEvent.getType(), new WizardRequiredEventHandler());
+  }
+
+  public void unbind() {
+    // TODO: This method is currently never being called (maybe it is not needed at all?). Note that
+    // WizardManager is a singleton whose bind() method is called by GwtApp.
+    if(handlerRegistration != null) {
+      handlerRegistration.removeHandler();
+    }
   }
 
   //
@@ -75,23 +98,10 @@ public class WizardManager {
     public void onWizardRequired(WizardRequiredEvent event) {
       unbindLastWizard();
 
-      switch(event.getWizardType()) {
-      case CREATE_DATASOURCE:
-        bindAndDisplayWizard(createDatasourceWizard.get(), event);
-        break;
-      case CREATE_VIEW:
-        bindAndDisplayWizard(createViewWizard.get(), event);
-        break;
-      case IMPORT_VARIABLES:
-        bindAndDisplayWizard(variablesImportWizard.get(), event);
-        break;
-      case IMPORT_DATA:
-        bindAndDisplayWizard(dataImportWizard.get(), event);
-        break;
-      case EXPORT_DATA:
-        bindAndDisplayWizard(dataExportWizard.get(), event);
-        break;
-      default:
+      Provider<? extends Wizard> wizardProvider = wizardProviders.get(event.getWizardType());
+      if(wizardProvider != null) {
+        bindAndDisplayWizard(wizardProvider.get(), event);
+      } else {
         throw new UnsupportedOperationException("wizard type not supported (" + event.getWizardType() + ")");
       }
     }
