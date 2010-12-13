f7ca04c8ec8579e771b03bf5003cee2a2ffbc672
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-12-13 16:06:50 +0000

    OPAL-960:   - Refresh datasources before attempting to updated the list of selectable ones. This prevents an NPE in production mode.

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/importdata/presenter/DestinationSelectionStepPresenter.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/importdata/presenter/DestinationSelectionStepPresenter.java
@@ -56,7 +56,7 @@ public class DestinationSelectionStepPresenter extends WidgetPresenter<Destinati
   public void setImportFormat(ImportFormat importFormat) {
     this.importFormat = importFormat;
     hideShowTables();
-    updateSelectableDatasources();
+    refreshDatasources();
   }
 
   @Override
