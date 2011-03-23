b10dd975e5f077ce25029451a0864fb4ff90103a
Philippe Laflamme <philippe.laflamme@obiba.org>
2011-03-23 20:22:58 +0000

    OPAL-1054 * Encapsulated DataSHIELD method 2

--- opal-r/src/main/R/opal/R/ds.R
+++ opal-r/src/main/R/opal/R/ds.R
@@ -56,8 +56,16 @@ datashield.aggregate=function(object, ...) {
 # Inner methods that sends a script, and aggregates the result using the specified aggregation method
 datashield.aggregate.opal=function(opal, aggregation, expr) {
   expression  = expr
-  if(is.language(expr)) expression = deparse(expr)
-  
+  # convert a call to a string
+  if(is.language(expr)) {
+    expression <- deparse(expr)
+    if(length(expression) > 1) {
+      expression = paste(expression, collapse='\n')
+    }
+  } else if(! is.character(expr) ) {
+    return(print(paste("Invalid expression type: '", class(value), "'. Expected a call or character vector.", sep="")))
+  }
+
   .post(opal, "datashield", "session", "current", "aggregate", aggregation, params=expression)
 }
 
@@ -70,15 +78,19 @@ datashield.assign=function(object, ...) {
 }
 
 datashield.assign.opal=function(opal, symbol, value) {
-  if(is.language(value)) {
+  if(is.language(value) || is.function(value)) {
     contentType <- "application/x-rscript"
     body <- deparse(value)
+    if(length(body) > 1) {
+      body = paste(body, collapse='\n')
+    }
   } else if(is.character(value)) {
     contentType <- "application/x-opal"
     body <- value
   } else {
     return(print(paste("Invalid value type: '", class(value), "'. Use quote() to protect from early evaluation.", sep="")))
   }
+
   .put(opal, "datashield", "session", "current", "symbol", symbol, body=body, contentType=contentType)
   # Return the new symbols length
   datashield.length(opal, symbol)
@@ -104,13 +116,13 @@ datashield.lm=function(object, ...) {
   UseMethod('datashield.lm');
 }
 
-datashield.lm.list=function(opals, formula, ...) {
+datashield.lm.list=function(opals, formula, lmparams=list()) {
 
   numstudies<-length(opals)
 
   # for Y ~ X + Z, terms will be c("X", "Z")
   # for Y ~ X * Z, terms will be c("X", "Z", "X:Z")
-  terms<-unlist(dimnames(attr(terms(formula), "factors"))[2])
+  terms<-attr(terms(formula), "term.labels")
 
   # numpara includes Intercept
   numpara<-length(terms)+1
@@ -118,10 +130,10 @@ datashield.lm.list=function(opals, formula, ...) {
   beta.s<-matrix(NA,nrow=numpara,ncol=numstudies)
   se.s<-matrix(NA,nrow=numpara,ncol=numstudies)
 
-  model<-substitute(lm(f), list(f=formula))
+  call<-as.call(c(quote(lm), formula, lmparams))
 
   for(k in 1:numstudies) {
-    study.summary<-datashield.aggregate(opals[[k]], "summary", model)
+    study.summary<-datashield.summary(opals[[k]], call)
     beta.s[,k]<-study.summary$coefficients[,1]
     se.s[,k]<-study.summary$coefficients[,2]
   }
@@ -130,20 +142,20 @@ datashield.lm.list=function(opals, formula, ...) {
   numsubs.study<-unlist(datashield.length(opals, terms[1]))
   numsubs<-sum(numsubs.study)
   analysis.wt<-numsubs.study/numsubs
-  
+
   #set up a vector of 1s to use in summing precisions
   simple.sum<-rep(1,numstudies)
-  
+
   #calculate mean of regression coefficients weighted for study sample sizes
   # “%*%” denotes vector multiplication 
   beta.overall<-beta.s%*%analysis.wt
-  
+
   #convert standard errors into precisions
   precision.s<-1/(se.s)^2
-  
+
   #sum precisions across studies
   precision.overall<-precision.s%*%simple.sum
-  
+
   #convert precisions back to standard errors
   se.overall<-1/(precision.overall)^0.5 
 
@@ -151,12 +163,142 @@ datashield.lm.list=function(opals, formula, ...) {
   meta.analysis.results<-cbind(beta.overall,se.overall)
 
   # Set dimension names
-  dimnames(meta.analysis.results)<-list(c("(Intercept)", terms),c("Coefficients","Std. Error"))
+  dimnames(meta.analysis.results)<-list(c("(Intercept)", terms),c("Coefficients","SE"))
 
   meta.analysis.results
 }
 
+# method required to add beta.vect to the result of the glm call.
+# beta.vect is then used in glm.ds aggregating function.
+# Alternatively, beta.vect could have been an additional paramater to the aggregating method, but is currently unsupported
+.glm.ds <-function(..., beta.vect=NULL) {
+  glm.result<-glm(..., x=TRUE, control=glm.control(maxit=1))
+  if(is.null(beta.vect)) {
+    X.mat<-as.matrix(glm.result$x)
+    beta.vect<-rep(0,dim(X.mat)[2])
+  }
+  glm.result$beta.vect<-beta.vect
+  glm.result
+}
 
-datashield.glm.list=function(opals, formula, ...) {
+datashield.glm.list=function(opals, formula, glmparams=list(), maxit=10) {
+
+  numstudies<-length(opals)
+  terms<-attr(terms(formula), "term.labels")
+
+  # Fetch the number of data points for each study 
+  nsubs.array<-as.array(unlist(datashield.length(opals, terms[1])))
+  nsubs.total<-sum(nsubs.array)
+
+  datashield.assign(opals, 'glm.ds', .glm.ds)
+
+  beta.vect.next<-NULL
   
+  #Iterations need to be counted. Start off with the count at 0
+  #and increment by 1 at each new iteration
+  iteration.count<-0
+
+  #Provide arbitrary starting value for deviance to enable subsequent calculation of the
+  #change in deviance between iterations
+  dev.old<-9.99e+99
+
+  #Convergence state needs to be monitored.
+  converge.state<-FALSE
+
+  #Define a convergence criterion. This value of epsilon corresponds to that used
+  #by default for GLMs in R (see section S3 for details)
+  epsilon<-1.0e-08
+
+  while(!converge.state && iteration.count < maxit) {
+    
+    iteration.count<-iteration.count+1
+    
+    cat("--------------------------------------------\n")
+    cat("Iteration", iteration.count, "\n")
+
+    glmparams$beta.vect<-as.vector(beta.vect.next)
+    call<-as.call(c('glm.ds', formula, glmparams))
+
+    study.summary<-datashield.aggregate(opals, 'glm.ds', call)
+
+    info.matrix.total<-Reduce(f="+", lapply(study.summary, function(s) {s[['info.matrix']]}))
+    score.vect.total<-Reduce(f="+", lapply(study.summary, function(s) {s[['score.vect']]}))
+    dev.total<-Reduce(f="+", lapply(study.summary, function(s) {s[['dev']]}))
+    
+    #Create variance covariance matrix as inverse of information matrix
+    #(solve() denotes matrix inversion in R )
+    variance.covariance.matrix.total<-solve(info.matrix.total)
+
+    #Create beta vector update terms (see subsection S2)
+    beta.update.vect<-variance.covariance.matrix.total %*% score.vect.total
+
+    #Add update terms to current beta vector to obtain new beta vector for next iteration
+    if(is.null(beta.vect.next)) {
+      beta.vect.next<-beta.update.vect
+    } else {
+      beta.vect.next<-beta.vect.next+beta.update.vect
+    }
+
+    #Calculate value of convergence statistic and test whether meets convergence criterion
+    #(see subsection S2)
+    converge.value<-abs(dev.total-dev.old)/(abs(dev.total)+0.1)
+    if(converge.value<=epsilon)converge.state<-TRUE
+    if(converge.value>epsilon)dev.old<-dev.total
+    
+    #For ALL iterations summarise model state after current iteration
+    cat("\nSUMMARY OF MODEL STATE after iteration No",iteration.count,
+        "\n\nCurrent deviance",dev.total,"on",
+        (nsubs.total-length(beta.vect.next)), "degrees of freedom",
+        "\nConvergence criterion    ",converge.state," (", converge.value,")\n\n")
+    
+    cat("Information matrix overall\n")
+    print(info.matrix.total)
+    
+    cat("Score vector overall\n")
+    print(score.vect.total)
+    
+    cat("Current Deviance\n")
+    print(dev.total)
+    cat("--------------------------------------------\n")    
+  }
+
+  #If convergence has been obtained, declare final (maximum likelihood) beta vector,
+  #and calculate the corresponding standard errors, z scores and p values
+  #(the latter two to be consistent with the output of a standard GLM analysis)
+  #Then print out final model summary
+  if(converge.state)
+  {
+    beta.vect.final<-beta.vect.next
+    se.vect.final<-sqrt(diag(variance.covariance.matrix.total))
+    z.vect.final<-beta.vect.final/se.vect.final
+    pval.vect.final<-2*pnorm(-abs(z.vect.final))
+    parameter.names<-names(score.vect.total[,1])
+    model.parameters<-cbind(beta.vect.final,se.vect.final,z.vect.final,pval.vect.final)
+    dimnames(model.parameters)<-list(parameter.names,c("Coefficient","SE","z-value","p-value"))
+
+    glmds <- list(
+        model.parameters=model.parameters,
+        dev=dev.total,
+        nsubs=nsubs.total,
+        df=(nsubs.total-length(beta.vect.next)),
+        iter=iteration.count
+    )
+
+    class(glmds) <- 'glmds'
+
+    glmds   
+  } else {
+    warning(paste("Did not converge after", maxit, "iterations. Increase maxit parameter as necessary."))
+    NULL
+  }
+}
+
+# Override print for the result of datashield.glm
+print.glmds <-function (x, digits = max(3, getOption("digits") - 3), ...) {
+  #If converged print out final model summary
+  cat("\n\nFINAL MODEL UNDER DATASHIELD\n")
+  print.default(signif(x$model.parameters, digits=digits), print.gap = 2, quote=FALSE)
+  cat("\nDeviance:",x$dev)
+  cat("\nDegrees of Freedom:", x$df)
+  cat("\nIterations:", x$iter, "\n")
 }
