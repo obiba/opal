3317108c485b60f7d35e8d29390de7b5644ae1db
Philippe Laflamme <philippe.laflamme@obiba.org>
2011-07-05 18:56:41 +0000

    Fixing Copyright notice for opal-r, opal-datashield and opal-search projects (all created in 2011).

--- opal-r/src/main/java/org/obiba/opal/r/service/VariableEntitiesHolder.java
+++ opal-r/src/main/java/org/obiba/opal/r/service/VariableEntitiesHolder.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
- * 
+ * Copyright (c) 2011 OBiBa. All rights reserved.
+ *  
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
- * 
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
