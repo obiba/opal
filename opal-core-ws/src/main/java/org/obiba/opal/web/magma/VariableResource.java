55f9e95949d4aca77f00d85356d9af73635259b5
Yannick Marcon <yannick.marcon@obiba.org>
2011-11-30 20:31:16 +0000

    Value resource added to variable resource: allows to get the individual value from a provided entity identifier. A binary value response is a downloadable file.

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/VariableResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/VariableResource.java
@@ -13,10 +13,21 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 
+import org.obiba.magma.NoSuchValueSetException;
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueSet;
 import org.obiba.magma.ValueTable;
+import org.obiba.magma.Variable;
+import org.obiba.magma.VariableEntity;
 import org.obiba.magma.VariableValueSource;
+import org.obiba.magma.support.VariableEntityBean;
+import org.obiba.magma.type.BinaryType;
 import org.obiba.opal.web.magma.support.DefaultPagingVectorSourceImpl;
 import org.obiba.opal.web.magma.support.InvalidRequestException;
 import org.obiba.opal.web.magma.support.PagingVectorSource;
@@ -43,6 +54,76 @@ public class VariableResource {
   @GET
   public VariableDto get() {
     return Dtos.asDto(vvs.getVariable()).build();
+  }
+
+  @GET
+  @Path("/value/{identifier}")
+  public Response getValue(@PathParam("identifier") String identifier, @QueryParam("pos") Integer pos) {
+    if(pos != null && pos < 0) return Response.status(Status.BAD_REQUEST).build();
+
+    Variable variable = vvs.getVariable();
+
+    if(!variable.isRepeatable() && pos != null) return Response.status(Status.BAD_REQUEST).build();
+
+    try {
+      Value value = extractValue(identifier);
+
+      ResponseBuilder builder;
+
+      if(value.isNull() || (value.isSequence() && pos != null && pos >= value.asSequence().getSize())) {
+        builder = Response.status(Status.NOT_FOUND);
+      } else {
+        value = getValueAt(value, pos);
+        if(value.isNull()) {
+          builder = Response.status(Status.NOT_FOUND);
+        } else {
+          builder = getValueResponse(identifier, value);
+        }
+      }
+
+      return builder.build();
+    } catch(NoSuchValueSetException ex) {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+  }
+
+  private Value extractValue(String identifier) {
+    Variable variable = vvs.getVariable();
+    VariableEntity entity = new VariableEntityBean(valueTable.getEntityType(), identifier);
+    ValueSet valueSet = valueTable.getValueSet(entity);
+    return valueTable.getValue(variable, valueSet);
+  }
+
+  private Value getValueAt(Value value, Integer pos) {
+    if(value.isSequence() && pos != null) return value.asSequence().get(pos);
+    else
+      return value;
+  }
+
+  private ResponseBuilder getValueResponse(String identifier, Value value) {
+    Variable variable = vvs.getVariable();
+    if(variable.getValueType().equals(BinaryType.get())) {
+      return getBinaryValueResponse(identifier, value);
+    }
+    return Response.ok(value.toString());
+  }
+
+  private ResponseBuilder getBinaryValueResponse(String identifier, Value value) {
+    if(value.isSequence()) return Response.status(Status.BAD_REQUEST);
+
+    Variable variable = vvs.getVariable();
+    ResponseBuilder builder;
+
+    // download as a file
+    if(variable.getMimeType() != null && !variable.getMimeType().isEmpty()) {
+      builder = Response.ok(value.getValue(), variable.getMimeType());
+    } else {
+      builder = Response.ok(value.getValue());
+    }
+    // TODO find a way to reverse mime-type to file extension
+    builder.header("Content-Disposition", "attachment; filename=\"" + variable.getName() + "-" + identifier + ".bin\"");
+
+    return builder;
   }
 
   @GET
