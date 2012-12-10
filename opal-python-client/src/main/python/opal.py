af1b9d0e67e781e8e14f7df32b16d4012def2041
Yannick Marcon <yannick.marcon@obiba.org>
2012-12-10 09:27:27 -0500

    opal-python-client added.

---
+++ opal-python-client/src/main/python/opal.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+#
+import opal.rest
+import opal.dictionary
+import opal.data
+import opal.file
+import argparse
+
+#
+# Add Opal access arguments
+#
+def add_opal_arguments(parser):
+  parser.add_argument('--opal', '-o', required=False, help='Opal server base url')
+  parser.add_argument('--user', '-u', required=False, help='User name')
+  parser.add_argument('--password', '-p', required=False, help='User password')
+  parser.add_argument('--verbose', '-v', action='store_true', help='Verbose output')
+
+def add_subcommand(name,help,add_args_func,default_func):
+  subparser = subparsers.add_parser(name, help=help)
+  add_opal_arguments(subparser)
+  add_args_func(subparser)
+  subparser.set_defaults(func=default_func)
+  
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Opal command line.')
+subparsers = parser.add_subparsers(title='sub-commands', help='Available sub-commands. Use --help option on the sub-command for more details.')
+
+# Add subcommands
+add_subcommand('dict', 'Query Opal data dictionary.', opal.dictionary.add_arguments,opal.dictionary.do_command)
+add_subcommand('data', 'Query Opal data.', opal.data.add_arguments,opal.data.do_command)
+add_subcommand('file', 'Query Opal file system.', opal.file.add_arguments,opal.file.do_command)
+add_subcommand('rest', 'Request directly the Opal REST API, for advanced users.', opal.rest.add_arguments,opal.rest.do_command)
+
+# Execute selected command
+args = parser.parse_args()
+args.func(args)
---
+++ opal-python-client/src/main/python/opal/rest.py
@@ -0,0 +1,95 @@
+"""
+Examples of Opal web services on which GET can be performed:
+
+/datasources
+  All datasources
+
+/datasource/xxx
+  A datasource
+
+/datasource/xxx/tables
+  All tables of a datasource
+
+/datasource/xxx/table/yyy
+  A table
+
+/datasource/xxx/table/yyy/variables
+  All variables of a table
+
+/datasource/xxx/table/yyy/variable/vvv
+  A variable
+
+/datasource/xxx/table/yyy/entities
+  All entities of a table
+
+/datasource/xxx/table/yyy/entities?script=sss
+  All entities of a table matching a script (see http://wiki.obiba.org/display/OPALDOC/Magma+Javascript+API)
+
+/datasource/xxx/table/yyy/valueSet/zzz
+  All values of a entity in a table
+
+/datasource/xxx/table/yyy/valueSet/zzz/variable/vvv
+  A variable value of a entity
+
+/datasource/xxx/table/yyy/valueSet/zzz/variable/vvv/value
+  Raw variable value of a entity
+
+/datasource/xxx/table/yyy/valueSet/zzz/variable/vvv/value?pos=1
+  Raw repeatable variable value of a entity at given position (start at 0)
+"""
+
+import sys
+import opal.core
+
+def add_arguments(parser):
+  """
+  Add REST command specific options
+  """
+  parser.add_argument('ws', help='Web service path, for instance: /datasource/xxx/table/yyy/variable/vvv')
+  parser.add_argument('--method', '-m', required=False, help='HTTP method (default is GET, others are POST, PUT, DELETE, OPTIONS)')
+  parser.add_argument('--accept', '-a', required=False, help='Accept header (default is application/json)')
+  parser.add_argument('--content-type', '-ct', required=False, help='Content-Type header (default is application/json)')
+  parser.add_argument('--json', '-j', action='store_true', help='Pretty JSON formatting of the response')
+  
+def do_command(args):
+  """
+  Execute REST command
+  """
+  # Build and send request
+  try:
+    request = opal.core.OpalClient(args).new_request()
+    request.fail_on_error()
+    
+    if args.accept:
+      request.accept(args.accept)
+    else:
+      request.accept_json()
+
+    if args.content_type:
+      request.content_type(args.content)
+      print 'Enter content:'
+      request.content(sys.stdin.read())
+
+    if args.verbose:
+      request.verbose()
+
+    # send request
+    request.method(args.method).resource(args.ws);
+    response = request.send()
+
+    # format response    
+    res = response.content
+    if args.json:
+      res = response.pretty_json()
+    elif args.method in ['OPTIONS']:
+      res = response.headers['Allow']
+
+    # output to stdout
+    print res
+  except Exception,e :
+    print e
+    sys.exit(2)
+  except pycurl.error, error:
+    errno, errstr = error
+    print >> sys.stderr, 'An error occurred: ', errstr
+    sys.exit(2)
