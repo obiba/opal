3579e3d9a70f8a67eba53515118a42ec04fd9d3b
Yannick Marcon <yannick.marcon@obiba.org>
2013-02-13 11:48:29 -0500

    DatasourceParsingException mapper added.

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/provider/DatasourceParsingExceptionMapper.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/provider/DatasourceParsingExceptionMapper.java
@@ -1,29 +1,32 @@
-/*******************************************************************************
- * Copyright 2008(c) The OBiBa Consortium. All rights reserved.
- * 
+/*
+ * Copyright (c) 2012 OBiBa. All rights reserved.
+ *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ */
 package org.obiba.opal.web.magma.provider;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.obiba.magma.NoSuchVariableException;
+import org.obiba.magma.support.DatasourceParsingException;
+import org.obiba.opal.web.magma.ClientErrorDtos;
 import org.springframework.stereotype.Component;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 @Component
 @Provider
-public class DatasourceParsingExceptionMapper implements ExceptionMapper<NoSuchVariableException> {
+public class DatasourceParsingExceptionMapper implements ExceptionMapper<DatasourceParsingException> {
 
   @Override
-  public Response toResponse(NoSuchVariableException exception) {
-    return Response.status(Status.NOT_FOUND).build();
+  public Response toResponse(DatasourceParsingException exception) {
+    return Response.status(BAD_REQUEST)
+        .entity(ClientErrorDtos.getErrorMessage(BAD_REQUEST, "DatasourceReadFailed", exception).build()).build();
   }
 
 }
