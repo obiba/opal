7765dc617d5ea1e5d2ff1594e85f2311d56089b5
Ramin Haeri Azad <rhaeri@p3g.org>
2013-02-27 19:19:46 -0500

    OPAL-1588 - added data loading view

---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/wizard/importdata/presenter/SpssFormatStepPresenter.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2012 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.obiba.opal.web.gwt.app.client.wizard.importdata.presenter;
+
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.place.Place;
+import net.customware.gwt.presenter.client.place.PlaceRequest;
+import net.customware.gwt.presenter.client.widget.WidgetDisplay;
+import net.customware.gwt.presenter.client.widget.WidgetPresenter;
+
+import org.obiba.opal.web.gwt.app.client.event.NotificationEvent;
+import org.obiba.opal.web.gwt.app.client.widgets.presenter.FileSelectionPresenter;
+import org.obiba.opal.web.gwt.app.client.widgets.presenter.FileSelectorPresenter.FileSelectionType;
+import org.obiba.opal.web.gwt.app.client.wizard.WizardStepDisplay;
+import org.obiba.opal.web.gwt.app.client.wizard.importdata.ImportData;
+import org.obiba.opal.web.gwt.app.client.wizard.importdata.ImportFormat;
+
+import com.google.inject.Inject;
+
+public class SpssFormatStepPresenter extends WidgetPresenter<SpssFormatStepPresenter.Display> implements DataImportPresenter.DataImportFormatStepPresenter {
+
+  @Inject
+  private FileSelectionPresenter spssFileSelectionPresenter;
+
+  @Inject
+  public SpssFormatStepPresenter(final Display display, final EventBus eventBus) {
+    super(display, eventBus);
+  }
+
+  @Override
+  public Place getPlace() {
+    return null;
+  }
+
+  @Override
+  protected void onBind() {
+    spssFileSelectionPresenter.setFileSelectionType(FileSelectionType.EXISTING_FILE_OR_FOLDER);
+    spssFileSelectionPresenter.bind();
+    getDisplay().setSpssFileSelectorWidgetDisplay(spssFileSelectionPresenter.getDisplay());
+  }
+
+  @Override
+  protected void onPlaceRequest(PlaceRequest request) {
+  }
+
+  @Override
+  protected void onUnbind() {
+  }
+
+  @Override
+  public void refreshDisplay() {
+  }
+
+  @Override
+  public void revealDisplay() {
+  }
+
+  @Override
+  public ImportData getImportData() {
+    ImportData importData = new ImportData();
+    importData.setFormat(ImportFormat.SPSS);
+    importData.setSpssFile(getDisplay().getSelectedFile());
+
+    return importData;
+  }
+
+  @Override
+  public boolean validate() {
+    if(getDisplay().getSelectedFile().isEmpty() || !getDisplay().getSelectedFile().toLowerCase().endsWith(".sav")) {
+      eventBus.fireEvent(NotificationEvent.newBuilder().error("SpssFileRequired").build());
+      return false;
+    }
+    return true;
+  }
+
+  //
+  // Interfaces
+  //
+
+  public interface Display extends WidgetDisplay, WizardStepDisplay {
+
+    void setSpssFileSelectorWidgetDisplay(FileSelectionPresenter.Display display);
+
+    String getSelectedFile();
+
+  }
+
+}
