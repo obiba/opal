c530933e9e9a8e36bb0ec223821dc4b998c8fe25
Etienne Morency-Bachand <emorency@p3g.org>
2013-04-03 14:38:04 -0400

    OPAL-1681:Fixed copyright

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/workbench/view/VariableSuggestOracle.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/workbench/view/VariableSuggestOracle.java
@@ -1,22 +1,15 @@
 /*
- * Copyright 2007 Google Inc.
+ * Copyright (c) 2013 OBiBa. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.obiba.opal.web.gwt.app.client.workbench.view;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -263,15 +256,6 @@ public class VariableSuggestOracle extends SuggestOracle {
             }
           }).send();
     }
-  }
-
-  /**
-   * Sets the default suggestion collection.
-   *
-   * @param suggestionList the default list of suggestions
-   */
-  public void setDefaultSuggestions(Collection<Suggestion> suggestionList) {
-    defaultResponse = new Response(suggestionList);
   }
 
   /**
