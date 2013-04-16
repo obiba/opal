69e7b8fdff216d2919b8a8387616e17f371757b1
Etienne Morency-Bachand <emorency@p3g.org>
2013-04-16 16:16:00 -0400

    OPAL-1769: Trying a different approach for stopping...

--- opal-search/src/main/java/org/obiba/opal/search/IndexSynchronizationManager.java
+++ opal-search/src/main/java/org/obiba/opal/search/IndexSynchronizationManager.java
@@ -84,8 +84,8 @@ public class IndexSynchronizationManager {
     currentTask.stop();
     syncProducer.deleteCurrentTaskFromQueue();
 
-    // reset currentTask
-    currentTask = null;
+//    // reset currentTask
+//    currentTask = null;
   }
 
   private Subject getSubject() {
