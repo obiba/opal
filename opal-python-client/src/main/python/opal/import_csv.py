bf74c20cc2a3756f4c10688ae43f00d0001f6f10
Etienne Morency-Bachand <emorency@p3g.org>
2013-05-28 14:46:27 -0400

    OPAL-1576: Set all csv import parameters (cherry picked from commit 8ab9b99)

--- opal-python-client/src/main/python/opal/import_csv.py
+++ opal-python-client/src/main/python/opal/import_csv.py
@@ -12,16 +12,16 @@ def add_arguments(parser):
     """
     Add data command specific options
     """
-    parser.add_argument('--path', '-pa', required=True, help='Local path of the CSV file to import.')
-    parser.add_argument('--characterSet', '-c', required=False, help='Character set')
+    parser.add_argument('--path', '-pa', required=True, help='CSV file to import on the Opal filesystem '
+                                                             '(from the Opal filesystem).')
+    parser.add_argument('--characterSet', '-c', required=False, help='Character set.')
     parser.add_argument('--separator', '-s', required=False, help='Field separator.')
-    parser.add_argument('--quote', '-q', required=False, help='Fields are enclosed by quotes.')
-    parser.add_argument('--firstRow', '-f', required=False, help='Skip first row as it contains column names.')
+    parser.add_argument('--quote', '-q', required=False, help='Quotation mark character.')
+    parser.add_argument('--firstRow', '-f', type=int, required=False, help='From row.')
 
     # non specific import arguments
     parser.add_argument('--destination', '-d', required=True, help='Destination datasource name')
-    parser.add_argument('--tables', '-t', nargs='+', required=False,
-                        help='The list of tables to be imported (defaults to all)')
+    parser.add_argument('--tables', '-t', nargs='+', required=False, help='Name of the table.')
     parser.add_argument('--incremental', '-i', action='store_true', help='Incremental import')
     parser.add_argument('--unit', '-un', required=False, help='Unit name for Participant ID mapping')
     parser.add_argument('--type', '-ty', required=True, help='Entity type (e.g. Participant)')
@@ -33,30 +33,34 @@ def add_csv_datasource_factory_extension(args, factory):
     Add specific datasource factory extension
     """
     csv_factory = factory.Extensions[opal.protobuf.Magma_pb2.CsvDatasourceFactoryDto.params]
-    # csv_factory.incremental = True
 
-    # if args.characterSet:
-    #     csv_factory.characterSet = args.characterSet
-    # else:
-    #     csv_factory.characterSet = 'utf-8'
+    if args.characterSet:
+        csv_factory.characterSet = args.characterSet
 
     if args.separator:
         csv_factory.separator = args.separator
+
+    if args.quote:
+        csv_factory.quote = args.quote
+
+    if args.firstRow:
+        csv_factory.firstRow = args.firstRow
+
+    table = csv_factory.tables.add()
+    table.data = args.path
+    table.entityType = args.type
+
+    if args.tables:
+        table.name = args.tables[0]
     else:
-        csv_factory.separator = ','
-        #
-        # if args.quote:
-        #     csv_factory.quote = args.quote
-        # else:
-        #     csv_factory.quote = ''
-        #
-        # if args.firstRow:
-        #     csv_factory.firstRow = args.firstRow
-        # else:
-        #     csv_factory.firstRow = True
+        # Take filename as the table name
+        name = args.path.split("/")
 
-
-        # name='bundle', full_name='Magma.CsvDatasourceFactoryDto.bundle', index=0,
+        index = name[-1].find('.csv')
+        if index > 0:
+            table.name = name[-1][:-index]
+        else:
+            table.name = name[-1]
 
 
 def do_command(args):
