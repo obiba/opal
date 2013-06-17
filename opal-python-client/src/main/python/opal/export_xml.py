2622c617c6f5ddff08ab4b17898edf93cd7fabd9
Etienne Morency-Bachand <emorency@p3g.org>
2013-06-17 15:48:00 -0400

    Fixed comment.

--- opal-python-client/src/main/python/opal/export_xml.py
+++ opal-python-client/src/main/python/opal/export_xml.py
@@ -28,7 +28,7 @@ def do_command(args):
         exporter = opal.io.OpalExporter.build(client=client, datasource=args.datasource, tables=args.tables,
                                               unit=args.unit, output=args.output, incremental=args.incremental,
                                               verbose=args.verbose)
-        # Take filename as the table name
+        # Check output filename extension
         if not (args.output.endswith('.zip')):
             raise Exception('Output must be a zip file.')
 
