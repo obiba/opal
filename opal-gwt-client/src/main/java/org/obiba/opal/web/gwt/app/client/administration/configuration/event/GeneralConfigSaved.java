1c85bdaa463f0a27e22ac639a34a1366b994b856
Etienne Morency-Bachand <emorency@p3g.org>
2013-11-06 10:42:41 -0500

    OPAL-1910: Using @GenEvent

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/administration/configuration/event/GeneralConfigSaved.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/administration/configuration/event/GeneralConfigSaved.java
@@ -3,7 +3,7 @@ package org.obiba.opal.web.gwt.app.client.administration.configuration.event;
 import com.gwtplatform.dispatch.annotation.GenEvent;
 
 /**
- * Will generate {@link org.obiba.opal.web.gwt.app.client.administration.configuration.event.GeneralConfigSavedEvent} and {@link org.obiba.opal.web.gwt.app.client.administration.configuration.event.GeneralConfigSavedHandler}
+ * Will generate {@link org.obiba.opal.web.gwt.app.client.administration.configuration.event.GeneralConfigSavedEvent} and {@link org.obiba.opal.web.gwt.app.client.administration.configuration.event.GeneralConfigSavedEvent.GeneralConfigSavedHandler}
  */
 @GenEvent
 public class GeneralConfigSaved {}
