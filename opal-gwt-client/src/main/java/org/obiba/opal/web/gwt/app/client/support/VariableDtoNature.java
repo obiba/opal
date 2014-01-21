3adfd409f2270eb27d7c36aefe85360212d1ed69
Etienne Morency-Bachand <emorency@p3g.org>
2014-01-21 09:43:41 -0500

    OPAL-2254: Applied the same logic to determine the variable natures as opal-core-api. Fixed some CSS

---
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/support/VariableDtoNature.java
@@ -0,0 +1,58 @@
+package org.obiba.opal.web.gwt.app.client.support;
+
+import org.obiba.opal.web.gwt.app.client.js.JsArrays;
+import org.obiba.opal.web.model.client.magma.CategoryDto;
+import org.obiba.opal.web.model.client.magma.VariableDto;
+
+/**
+ * Determines the nature of a variable by inspecting its {@code ValueType} and any associated {@code Category} instance.
+ */
+public enum VariableDtoNature {
+  /**
+   * A categorical variable: its value can take one of the predefined {@code Category}.
+   */
+  CATEGORICAL,
+
+  /**
+   * A continuous variable: its value can take any value of it's {@code ValueType}. Some values may have a particular
+   * meaning: they indicate a missing value. These are defined as missing {@code Category} instances.
+   */
+  CONTINUOUS,
+
+  /**
+   * A temporal variable: it's value is a date or time.
+   */
+  TEMPORAL,
+
+  /**
+   * None of the above. Variables with {@code LocaleType} will be of this nature.
+   */
+  UNDETERMINED;
+
+  public static VariableDtoNature getNature(VariableDto variable) {
+
+    if(JsArrays.toSafeArray(variable.getCategoriesArray()).length() > 0) {
+      return areAllCategoriesMissing(variable) ? CONTINUOUS : CATEGORICAL;
+    }
+    if("integer".equals(variable.getValueType()) || "double".equals(variable.getValueType())) {
+      return CONTINUOUS;
+    }
+    if("date".equals(variable.getValueType()) || "datetime".equals(variable.getValueType())) {
+      return TEMPORAL;
+    }
+    if("boolean".equals(variable.getValueType())) {
+      return CATEGORICAL;
+    }
+    return UNDETERMINED;
+  }
+
+  private static boolean areAllCategoriesMissing(VariableDto variable) {
+    for(CategoryDto categoryDto : JsArrays.toIterable(variable.getCategoriesArray())) {
+      if(!categoryDto.getIsMissing()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
