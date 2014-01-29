9545ea0d6383ab0a611295231ae9ac506d4faeab
Cedric Thiebault <cedric.thiebault@gmail.com>
2014-01-29 11:40:08 -0500

    Fix bug that prevent to create user because of SubjectProfileNotFoundException

--- opal-core/src/main/java/org/obiba/opal/core/service/security/SubjectCredentialsServiceImpl.java
+++ opal-core/src/main/java/org/obiba/opal/core/service/security/SubjectCredentialsServiceImpl.java
@@ -27,6 +27,7 @@ import org.obiba.opal.core.domain.security.SubjectProfile;
 import org.obiba.opal.core.security.OpalKeyStore;
 import org.obiba.opal.core.service.DuplicateSubjectProfileException;
 import org.obiba.opal.core.service.OrientDbService;
+import org.obiba.opal.core.service.SubjectProfileNotFoundException;
 import org.obiba.opal.core.service.SubjectProfileService;
 import org.obiba.opal.core.service.security.realm.ApplicationRealm;
 import org.obiba.opal.core.service.security.realm.OpalUserRealm;
@@ -235,9 +236,13 @@ public class SubjectCredentialsServiceImpl implements SubjectCredentialsService 
    */
   private void validateProfile(SubjectCredentials subjectCredentials) {
     String realm = getRealmFromType(subjectCredentials.getAuthenticationType());
-    SubjectProfile profile = subjectProfileService.getProfile(subjectCredentials.getName());
-    if(!realm.equals(profile.getRealm())) {
-      throw new DuplicateSubjectProfileException(profile);
+    try {
+      SubjectProfile profile = subjectProfileService.getProfile(subjectCredentials.getName());
+      if(!realm.equals(profile.getRealm())) {
+        throw new DuplicateSubjectProfileException(profile);
+      }
+    } catch(SubjectProfileNotFoundException ignored) {
+      // do nothing as this principal has no profile
     }
   }
 
