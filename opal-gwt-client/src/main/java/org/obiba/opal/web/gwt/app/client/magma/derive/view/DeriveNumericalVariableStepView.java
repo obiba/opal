e041542afe6308f2f9831358be41ae37cbfe4c81
Etienne Morency-Bachand <emorency@p3g.org>
2014-02-05 10:30:41 -0500

    OPAL-2337: Fixing derivation problem for numerical when changing categories and apply the same style for the add new value mapping section as temporal derivation

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/magma/derive/view/DeriveNumericalVariableStepView.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/magma/derive/view/DeriveNumericalVariableStepView.java
@@ -131,6 +131,7 @@ public class DeriveNumericalVariableStepView extends ViewWithUiHandlers<Derivati
     lengthRadio.setValue(true, true);
     rangeRadio.setValue(true, true);
     addRangeRadio.setValue(true, true);
+
   }
 
   private void initializeValueMapEntryForm() {
@@ -170,7 +171,7 @@ public class DeriveNumericalVariableStepView extends ViewWithUiHandlers<Derivati
     getUiHandlers().onMethodChange();
   }
 
-  @UiHandler({ "lengthBox", "countBox", "addFromBox", "addToBox" })
+  @UiHandler({ "lengthBox", "countBox", "fromBox", "toBox" })
   void onMethodChanged(KeyUpEvent event) {
     getUiHandlers().onMethodChange();
   }
