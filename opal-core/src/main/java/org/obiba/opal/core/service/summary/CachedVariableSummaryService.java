d0c258695ce35e4a2e245a634217f47fe60dbdc9
Etienne Morency-Bachand <emorency@p3g.org>
2014-02-20 14:45:47 -0500

    Fixing wrong index name

--- opal-core/src/main/java/org/obiba/opal/core/service/summary/CachedVariableSummaryService.java
+++ opal-core/src/main/java/org/obiba/opal/core/service/summary/CachedVariableSummaryService.java
@@ -160,7 +160,7 @@ public class CachedVariableSummaryService implements VariableSummaryService {
     @NotNull
     @Override
     protected Cache getCache() {
-      return cacheManager.getCache("opal-variable-summary-default");
+      return cacheManager.getCache("opal-variable-summary-binary");
     }
 
     @NotNull
