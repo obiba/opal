8da1663b3e1fe81a66881928e4d5ea2055be5919
Cedric Thiebault <cthiebault@p3g.org>
2014-02-21 11:07:07 -0500

    Extends EhCacheRegionFactory to set cache name

--- opal-core/src/main/java/org/obiba/opal/core/runtime/jdbc/SessionFactoryFactory.java
+++ opal-core/src/main/java/org/obiba/opal/core/runtime/jdbc/SessionFactoryFactory.java
@@ -9,15 +9,25 @@
  ******************************************************************************/
 package org.obiba.opal.core.runtime.jdbc;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
 
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.ConfigurationFactory;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory;
+import org.hibernate.cache.CacheException;
+import org.hibernate.cache.ehcache.EhCacheRegionFactory;
+import org.hibernate.cache.ehcache.internal.util.HibernateUtil;
+import org.hibernate.cfg.Settings;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.HSQLDialect;
 import org.hibernate.service.classloading.internal.ClassLoaderServiceImpl;
@@ -26,6 +36,8 @@ import org.hibernate.service.jdbc.dialect.internal.StandardDialectResolver;
 import org.obiba.magma.datasource.hibernate.cfg.HibernateConfigurationHelper;
 import org.obiba.magma.datasource.hibernate.cfg.MagmaHSQLDialect;
 import org.obiba.magma.datasource.hibernate.cfg.MagmaNamingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.DataAccessException;
@@ -40,6 +52,8 @@ import static org.hibernate.cfg.AvailableSettings.*;
 
 @Component
 public class SessionFactoryFactory {
+
+  private static final Logger log = LoggerFactory.getLogger(SessionFactoryFactory.class);
 
   @Autowired
   private ApplicationContext applicationContext;
@@ -69,8 +83,7 @@ public class SessionFactoryFactory {
     factory.getHibernateProperties().setProperty(USE_STRUCTURED_CACHE, "true");
     factory.getHibernateProperties().setProperty(USE_QUERY_CACHE, "true");
     factory.getHibernateProperties().setProperty(USE_SECOND_LEVEL_CACHE, "true");
-//    factory.getHibernateProperties().setProperty(CACHE_REGION_FACTORY, EhCacheRegionFactory.class.getName());
-    factory.getHibernateProperties().setProperty(CACHE_REGION_FACTORY, SingletonEhCacheRegionFactory.class.getName());
+    factory.getHibernateProperties().setProperty(CACHE_REGION_FACTORY, NamedEhCacheRegionFactory.class.getName());
     factory.getHibernateProperties().setProperty(CURRENT_SESSION_CONTEXT_CLASS, "jta");
     factory.getHibernateProperties().setProperty(AUTO_CLOSE_SESSION, "true");
     factory.getHibernateProperties().setProperty(FLUSH_BEFORE_COMPLETION, "true");
@@ -87,6 +100,67 @@ public class SessionFactoryFactory {
       }
     });
     return dialect instanceof HSQLDialect ? MagmaHSQLDialect.class.getName() : dialect.getClass().getName();
+  }
+
+  /**
+   * Used to set name to cache manager to avoid net.sf.ehcache.CacheException:
+   * <pre>
+   *   net.sf.ehcache.CacheException:
+   *   Another unnamed CacheManager already exists in the same VM. Please provide unique names for each CacheManager in the config or do one of following:
+   *    1. Use one of the CacheManager.create() static factory methods to reuse same CacheManager with same name or create one if necessary
+   *    2. Shutdown the earlier cacheManager before creating new one with same name.
+   * </pre>
+   * <p/>
+   * See https://hibernate.atlassian.net/browse/HHH-7809
+   */
+  public static class NamedEhCacheRegionFactory extends EhCacheRegionFactory {
+
+    private static final long serialVersionUID = -906012674778107555L;
+
+    @Override
+    @SuppressWarnings({ "ParameterHidesMemberVariable", "OverlyLongMethod", "PMD.NcssMethodCount" })
+    public void start(Settings settings, Properties properties) throws CacheException {
+      this.settings = settings;
+      if(manager != null) {
+        log.warn(
+            "Attempt to restart an already started EhCacheProvider. Use sessionFactory.close() between repeated calls to " +
+                "buildSessionFactory. Using previously created EhCacheProvider. If this behaviour is required, consider " +
+                "using net.sf.ehcache.hibernate.SingletonEhCacheProvider.");
+        return;
+      }
+
+      try {
+        String configurationResourceName = null;
+        if(properties != null) {
+          configurationResourceName = (String) properties.get(NET_SF_EHCACHE_CONFIGURATION_RESOURCE_NAME);
+        }
+        if(configurationResourceName == null || configurationResourceName.isEmpty()) {
+          Configuration configuration = ConfigurationFactory.parseConfiguration();
+          // give the CacheManager some unique name. we could also use UUID or whatever
+          configuration.setName(toString());
+          manager = new CacheManager(configuration);
+        } else {
+          URL url;
+          try {
+            url = new URL(configurationResourceName);
+          } catch(MalformedURLException e) {
+            url = loadResource(configurationResourceName);
+          }
+          Configuration configuration = HibernateUtil.loadAndCorrectConfiguration(url);
+          manager = new CacheManager(configuration);
+        }
+        mbeanRegistrationHelper.registerMBean(manager, properties);
+      } catch(net.sf.ehcache.CacheException e) {
+        if(e.getMessage().startsWith("Cannot parseConfiguration CacheManager. Attempt to create a new instance of " +
+            "CacheManager using the diskStorePath")) {
+          throw new CacheException("Attempt to restart an already started EhCacheRegionFactory. " +
+              "Use sessionFactory.close() between repeated calls to buildSessionFactory. " +
+              "Consider using SingletonEhCacheRegionFactory. Error from ehcache was: " + e.getMessage());
+        } else {
+          throw new CacheException(e);
+        }
+      }
+    }
   }
 
 }
