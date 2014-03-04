079f167daeed52b0e1f94f93d532f8c6d016503c
Ramin Haeri Azad <rhaeri@p3g.org>
2014-03-04 13:32:55 -0500

    OPAL-2520 - added back the POST method

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/math/DefaultSummaryResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/math/DefaultSummaryResource.java
@@ -2,12 +2,14 @@ package org.obiba.opal.web.magma.math;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 public interface DefaultSummaryResource extends SummaryResource {
 
   @GET
+  @POST // requires POST since the request body contains variable info (categories, script, etc)
   Response get(@QueryParam("offset") Integer offset, //
       @QueryParam("limit") Integer limit, //
       @QueryParam("fullIfCached") @DefaultValue("false") boolean fullIfCached, //
