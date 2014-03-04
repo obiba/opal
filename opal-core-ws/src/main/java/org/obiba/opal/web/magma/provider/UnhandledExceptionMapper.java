b1f1e52617fe3cc4e054e2ba91bfcaf3d0c21fa1
Ramin Haeri Azad <rhaeri@p3g.org>
2014-03-03 21:39:11 -0500

    ExceptionMapping - changed RuntimeException to Exception

--- opal-core-ws/src/main/java/org/obiba/opal/web/magma/provider/UnhandledExceptionMapper.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/provider/UnhandledExceptionMapper.java
@@ -1,12 +1,12 @@
-/*******************************************************************************
- * Copyright (c) 2011 OBiBa. All rights reserved.
+/*
+ * Copyright (c) 2014 OBiBa. All rights reserved.
  *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ */
 package org.obiba.opal.web.magma.provider;
 
 import javax.ws.rs.core.Response;
@@ -22,12 +22,12 @@ import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 @Component
 @Provider
-public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+public class UnhandledExceptionMapper implements ExceptionMapper<Exception> {
 
-  private static final Logger log = LoggerFactory.getLogger(RuntimeExceptionMapper.class);
+  private static final Logger log = LoggerFactory.getLogger(UnhandledExceptionMapper.class);
 
   @Override
-  public Response toResponse(RuntimeException exception) {
+  public Response toResponse(Exception exception) {
     log.error("Unhandled exception", exception);
     return Response.status(BAD_REQUEST)
         .entity(ClientErrorDtos.getErrorMessage(BAD_REQUEST, "UnhandledException", exception.getMessage()).build())
