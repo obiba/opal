b3ce5584a3ded66d011b515fb35745cc236434dd
Cedric Thiebault <cthiebault@p3g.org>
2014-03-17 15:32:04 -0400

    * Set version to 2.0.2-SNAPSHOT * Fix OPAL-2552: Attributes of variables and categories are lost after upgrading opal

--- opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/FixAttributeJoinTableUpgradeStep.java
+++ opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/FixAttributeJoinTableUpgradeStep.java
@@ -10,6 +10,10 @@
 
 package org.obiba.opal.core.upgrade.v2_0_x.database;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.hibernate.LockMode;
@@ -26,8 +30,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ConnectionCallback;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
 public class FixAttributeJoinTableUpgradeStep extends AbstractUpgradeStep {
@@ -37,13 +45,15 @@ public class FixAttributeJoinTableUpgradeStep extends AbstractUpgradeStep {
   @Autowired
   private DatabaseRegistry databaseRegistry;
 
+  @Autowired
+  private TransactionTemplate transactionTemplate;
+
   @Override
   public void execute(Version currentVersion) {
     process(databaseRegistry.getIdentifiersDatabase());
     for(Database database : databaseRegistry.listSqlDatabases()) {
       process(database);
     }
-
   }
 
   private void process(Database database) {
@@ -56,38 +66,92 @@ public class FixAttributeJoinTableUpgradeStep extends AbstractUpgradeStep {
 
     JdbcOperations jdbcTemplate = new JdbcTemplate(databaseRegistry.getDataSource(database.getName(), null));
 
-    alter(jdbcTemplate, "ALTER TABLE datasource_state_attributes RENAME TO datasource_attributes");
-    alter(jdbcTemplate, "ALTER TABLE datasource_attributes RENAME COLUMN datasource_state_id to datasource_id");
-    alter(jdbcTemplate, "ALTER TABLE datasource_attributes RENAME COLUMN datasource to datasource_id");
-
-    alter(jdbcTemplate, "ALTER TABLE variable_state_attributes RENAME TO variable_attributes");
-    alter(jdbcTemplate, "ALTER TABLE variable_attributes RENAME COLUMN variable_state_id to variable_id");
-    alter(jdbcTemplate, "ALTER TABLE variable_attributes RENAME COLUMN variable to variable_id");
-
-    alter(jdbcTemplate, "ALTER TABLE category_state_attributes RENAME TO category_attributes");
-    alter(jdbcTemplate, "ALTER TABLE category_attributes RENAME COLUMN category_state_id to category_id");
-    alter(jdbcTemplate, "ALTER TABLE category_attributes RENAME COLUMN category to category_id");
+    switch(database.getSqlSettings().getDriverClass()) {
+      case "com.mysql.jdbc.Driver":
+        alterMySql(jdbcTemplate);
+        break;
+      case "org.hsqldb.jdbc.JDBCDriver":
+        alterHsql(jdbcTemplate);
+        break;
+    }
 
     touchValueTablesTimestamps(database);
   }
 
-  private void alter(JdbcOperations jdbcTemplate, String sql) {
-    try {
-      log.debug("Execute '{}'", sql);
-      jdbcTemplate.execute(sql);
-    } catch(DataAccessException e) {
-      log.debug("Ignore error during '{}'", sql, e);
-    }
+  private void alterMySql(JdbcOperations jdbcTemplate) {
+    alter(jdbcTemplate, "ALTER TABLE datasource_state_attributes RENAME TO datasource_attributes");
+    alter(jdbcTemplate,
+        "ALTER TABLE datasource_attributes DROP FOREIGN KEY " + getForeignKey(jdbcTemplate, "datasource_attributes"));
+    alter(jdbcTemplate,
+        "ALTER TABLE datasource_attributes CHANGE COLUMN datasource_state_id datasource_id bigint(20) NOT NULL");
+    alter(jdbcTemplate, "ALTER TABLE datasource_attributes CHANGE COLUMN datasource datasource_id bigint(20) NOT NULL");
+
+    alter(jdbcTemplate, "ALTER TABLE variable_state_attributes RENAME TO variable_attributes");
+    alter(jdbcTemplate,
+        "ALTER TABLE variable_attributes DROP FOREIGN KEY " + getForeignKey(jdbcTemplate, "variable_attributes"));
+    alter(jdbcTemplate,
+        "ALTER TABLE variable_attributes CHANGE COLUMN variable_state_id variable_id bigint(20) NOT NULL");
+    alter(jdbcTemplate, "ALTER TABLE variable_attributes CHANGE COLUMN variable variable_id bigint(20) NOT NULL");
+
+    alter(jdbcTemplate, "ALTER TABLE category_state_attributes RENAME TO category_attributes");
+    alter(jdbcTemplate,
+        "ALTER TABLE category_attributes DROP FOREIGN KEY " + getForeignKey(jdbcTemplate, "category_attributes"));
+    alter(jdbcTemplate,
+        "ALTER TABLE category_attributes CHANGE COLUMN category_state_id category_id bigint(20) NOT NULL");
+    alter(jdbcTemplate, "ALTER TABLE category_attributes CHANGE COLUMN category category_id bigint(20) NOT NULL");
+  }
+
+  private void alterHsql(JdbcOperations jdbcTemplate) {
+    alter(jdbcTemplate, "ALTER TABLE datasource_state_attributes RENAME TO datasource_attributes");
+    alter(jdbcTemplate, "ALTER TABLE datasource_attributes ALTER COLUMN datasource_state_id RENAME TO datasource_id");
+    alter(jdbcTemplate, "ALTER TABLE datasource_attributes ALTER COLUMN datasource RENAME TO datasource_id");
+    alter(jdbcTemplate, "ALTER TABLE variable_state_attributes RENAME TO variable_attributes");
+    alter(jdbcTemplate, "ALTER TABLE variable_attributes ALTER COLUMN variable_state_id RENAME TO variable_id");
+    alter(jdbcTemplate, "ALTER TABLE variable_attributes ALTER COLUMN variable RENAME TO variable_id");
+
+    alter(jdbcTemplate, "ALTER TABLE category_state_attributes RENAME TO category_attributes");
+    alter(jdbcTemplate, "ALTER TABLE category_attributes ALTER COLUMN category_state_id RENAME TO category_id");
+    alter(jdbcTemplate, "ALTER TABLE category_attributes ALTER COLUMN category RENAME TO category_id");
+  }
+
+  private String getForeignKey(JdbcOperations jdbcTemplate, final String tableName) {
+    return jdbcTemplate.execute(new ConnectionCallback<String>() {
+      @Override
+      public String doInConnection(Connection connection) throws SQLException, DataAccessException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet rs = metaData.getImportedKeys(null, null, tableName);
+        return rs.next() ? rs.getString(12) : null;
+      }
+    });
+  }
+
+  private void alter(final JdbcOperations jdbcTemplate, final String sql) {
+    transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+      @Override
+      protected void doInTransactionWithoutResult(TransactionStatus status) {
+        try {
+          jdbcTemplate.execute(sql);
+          log.debug("Executed '{}'", sql);
+        } catch(DataAccessException ignored) {
+        }
+      }
+    });
   }
 
   @SuppressWarnings("unchecked")
-  private void touchValueTablesTimestamps(Database database) {
-    SessionFactory sessionFactory = databaseRegistry.getSessionFactory(database.getName(), null);
-    Session currentSession = sessionFactory.getCurrentSession();
-    for(ValueTableState table : (List<ValueTableState>) currentSession.createCriteria(ValueTableState.class).list()) {
-      log.debug("Touch {} last update", table.getName());
-      currentSession.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_FORCE_INCREMENT)).lock(table);
-    }
+  private void touchValueTablesTimestamps(final Database database) {
+    transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+      @Override
+      protected void doInTransactionWithoutResult(TransactionStatus status) {
+        SessionFactory sessionFactory = databaseRegistry.getSessionFactory(database.getName(), null);
+        Session currentSession = sessionFactory.getCurrentSession();
+        for(ValueTableState table : (List<ValueTableState>) currentSession.createCriteria(ValueTableState.class)
+            .list()) {
+          log.debug("Touch {} last update", table.getName());
+          currentSession.buildLockRequest(new LockOptions(LockMode.PESSIMISTIC_FORCE_INCREMENT)).lock(table);
+        }
+      }
+    });
   }
 
 }
