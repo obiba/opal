fdb1b18081295e87e5230d52c97608789d89278d
Cedric Thiebault <cthiebault@p3g.org>
2014-03-18 14:42:34 -0400

    Drop version table for 2.0.2

--- opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/DropVersionTableUpgradeStep.java
+++ opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/DropVersionTableUpgradeStep.java
@@ -3,18 +3,27 @@ package org.obiba.opal.core.upgrade.v2_0_x.database;
 import org.obiba.opal.core.service.database.DatabaseRegistry;
 import org.obiba.runtime.Version;
 import org.obiba.runtime.upgrade.AbstractUpgradeStep;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
 public class DropVersionTableUpgradeStep extends AbstractUpgradeStep {
+
+  private static final Logger log = LoggerFactory.getLogger(DropVersionTableUpgradeStep.class);
 
   @Autowired
   private DatabaseRegistry databaseRegistry;
 
   @Override
   public void execute(Version currentVersion) {
-    new JdbcTemplate(databaseRegistry.getDataSource("opal-data", null)).execute("drop table version");
+    try {
+      new JdbcTemplate(databaseRegistry.getDataSource("opal-data", null)).execute("drop table version");
+    } catch(DataAccessException e) {
+      log.debug("Ignore error while dropping version table", e);
+    }
   }
 
 }
