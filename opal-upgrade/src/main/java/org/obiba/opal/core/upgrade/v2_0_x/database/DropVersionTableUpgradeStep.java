fc9f11996e3368b42160d763a3857635ded1bb80
Yannick Marcon <yannick.marcon@obiba.org>
2014-03-18 21:16:44 -0400

    OPAL-2557 Ignore all exceptions

--- opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/DropVersionTableUpgradeStep.java
+++ opal-upgrade/src/main/java/org/obiba/opal/core/upgrade/v2_0_x/database/DropVersionTableUpgradeStep.java
@@ -1,12 +1,12 @@
 package org.obiba.opal.core.upgrade.v2_0_x.database;
 
 import org.obiba.opal.core.service.database.DatabaseRegistry;
+import org.obiba.opal.core.service.database.NoSuchDatabaseException;
 import org.obiba.runtime.Version;
 import org.obiba.runtime.upgrade.AbstractUpgradeStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
@@ -21,7 +21,9 @@ public class DropVersionTableUpgradeStep extends AbstractUpgradeStep {
   public void execute(Version currentVersion) {
     try {
       new JdbcTemplate(databaseRegistry.getDataSource("opal-data", null)).execute("drop table version");
-    } catch(DataAccessException e) {
+    } catch (NoSuchDatabaseException e) {
+      // does not apply to this instance
+    } catch(Exception e) {
       log.debug("Ignore error while dropping version table", e);
     }
   }
