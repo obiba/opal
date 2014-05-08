255102196ac2cd174f4f2033d1c3d99044571695
Yannick Marcon <yannick.marcon@obiba.org>
2014-05-08 11:58:27 -0400

    OPAL-2594 Variable permission command added.

---
+++ opal-python-client/src/main/python/opal/perm_variable.py
@@ -0,0 +1,62 @@
+"""
+Apply permissions on a set of tables.
+"""
+
+import sys
+import opal.core
+import opal.perm
+
+PERMISSIONS = {
+    'view': 'VARIABLE_READ',
+}
+
+
+def add_arguments(parser):
+    """
+    Add command specific options
+    """
+    opal.perm.add_permission_arguments(parser, PERMISSIONS.keys())
+    parser.add_argument('--project', '-pr', required=True, help='Project name to which the tables belong')
+    parser.add_argument('--table', '-t', required=True, help='Table name to which the variables belong')
+    parser.add_argument('--variables', '-va', nargs='+', required=True,
+                        help='List of variable names on which the permission is to be set')
+
+
+def do_command(args):
+    """
+    Execute permission command
+    """
+    # Build and send requests
+    try:
+        opal.perm.validate_args(args, PERMISSIONS)
+
+        for variable in args.variables:
+            request = opal.core.OpalClient.build(opal.core.OpalClient.LoginInfo.parse(args)).new_request()
+
+            if args.verbose:
+                request.verbose()
+
+            # send request
+            if args.delete:
+                request.delete()
+            else:
+                request.post()
+
+            try:
+                response = request.resource(
+                    opal.perm.do_ws(args, ['project', args.project, 'permissions', 'table', args.table, 'variable', variable], PERMISSIONS)).send()
+            except Exception, e:
+                print Exception, e
+
+            # format response
+            if response.code != 200:
+                print response.content
+
+    except Exception, e:
+        print e
+        sys.exit(2)
+
+    except pycurl.error, error:
+        errno, errstr = error
+        print >> sys.stderr, 'An error occurred: ', errstr
+        sys.exit(2)
