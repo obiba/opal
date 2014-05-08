d51faf5243a07c02c35576833d31f8b202bc3d6b
Yannick Marcon <yannick.marcon@obiba.org>
2014-05-08 11:06:24 -0400

    OPAL-2594 Added commands for setting project and datasource permissions

---
+++ opal-python-client/src/main/python/opal/perm_datasource.py
@@ -0,0 +1,55 @@
+"""
+Apply permissions on a set of tables.
+"""
+
+import sys
+import opal.core
+import opal.perm
+
+PERMISSIONS = {
+    'add-table': 'TABLE_ADD',
+    'administrate': 'DATASOURCE_ALL'
+}
+
+def add_arguments(parser):
+    """
+    Add command specific options
+    """
+    opal.perm.add_permission_arguments(parser, PERMISSIONS.keys())
+    parser.add_argument('--project', '-pr', required=True, help='Project name to which the tables belong')
+
+def do_command(args):
+    """
+    Execute permission command
+    """
+    # Build and send requests
+    try:
+        opal.perm.validate_args(args, PERMISSIONS)
+
+        request = opal.core.OpalClient.build(opal.core.OpalClient.LoginInfo.parse(args)).new_request()
+
+        if args.verbose:
+            request.verbose()
+
+        # send request
+        if args.delete:
+            request.delete()
+        else:
+            request.post()
+
+        try:
+            response = request.resource(opal.perm.do_ws(args, ['project', args.project, 'permissions', 'datasource'], PERMISSIONS)).send()
+        except Exception, e:
+            print Exception, e
+
+        # format response
+        print response.content
+
+    except Exception, e:
+        print e
+        sys.exit(2)
+
+    except pycurl.error, error:
+        errno, errstr = error
+        print >> sys.stderr, 'An error occurred: ', errstr
+        sys.exit(2)
