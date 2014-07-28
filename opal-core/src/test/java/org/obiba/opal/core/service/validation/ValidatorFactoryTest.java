8d7fd2f1386aed08009515eed2e22d7dc33b7ebb
Carlos Silva <carlos@Carloss-MacBook-Pro.local>
2014-07-29 01:42:14 +0200

    Fixing ValidatorFactoryTest

--- opal-core/src/test/java/org/obiba/opal/core/service/validation/ValidatorFactoryTest.java
+++ opal-core/src/test/java/org/obiba/opal/core/service/validation/ValidatorFactoryTest.java
@@ -1,8 +1,13 @@
 package org.obiba.opal.core.service.validation;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueType;
+import org.obiba.magma.type.TextType;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -21,23 +26,21 @@ public class ValidatorFactoryTest {
 
     @Test
     public void testFileVocab() throws Exception {
-        //String url = "file://foo.csv";
-        URI uri = new URI("file", "/", "aaaaaaa.csv");
+        //File file = new File("opal-core/src/test/resources/vocabulary.csv");
+        File file = new File("src/test/resources/vocabulary.csv");
+        URI uri = file.toURI();
         String url = uri.toURL().toExternalForm();
-        System.out.println(url);
-        //InputStream in = new URL(url).openStream();
-        //System.out.println(in);
-
 
         validateVocab(factory.getVocabularyValidator(url));
-
     }
 
     private void validateVocab(VocabularyValidator validator) {
-
-        System.out.println(validator);
-
-
+        Assert.assertTrue(validator.isValid(TextType.get().valueOf("AAA")));
+        Assert.assertTrue(validator.isValid(TextType.get().valueOf("BBB")));
+        Assert.assertTrue(validator.isValid(TextType.get().valueOf("CCC")));
+        Assert.assertFalse(validator.isValid(TextType.get().valueOf("aaa")));
+        Assert.assertFalse(validator.isValid(TextType.get().valueOf("foo")));
+        Assert.assertFalse(validator.isValid(TextType.get().valueOf("bar")));
     }
 
 }
