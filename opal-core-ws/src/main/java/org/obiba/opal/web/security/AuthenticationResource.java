9a1e2a499c2b43c85bc9faa3faf7411eb90242e9
Yannick Marcon <yannick.marcon@obiba.org>
2014-09-24 11:36:43 -0400

    OPAL-2662 Successfull login IP reported in log

--- opal-core-ws/src/main/java/org/obiba/opal/web/security/AuthenticationResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/security/AuthenticationResource.java
@@ -59,7 +59,8 @@ public class AuthenticationResource extends AbstractSecurityComponent {
       ThreadContext.bind(subject);
       ensureProfile(subject);
       String sessionId = SecurityUtils.getSubject().getSession().getId().toString();
-      log.info("Successful session creation for user '{}' session ID is '{}'.", username, sessionId);
+      log.info("Successful session creation for user '{}' at ip: '{}': session ID is '{}'.", username,
+          servletRequest.getRemoteAddr(), sessionId);
       return Response.created(
           UriBuilder.fromPath("/").path(AuthenticationResource.class).path(AuthenticationResource.class, "checkSession")
               .build(sessionId)).build();
