7b9e5cbca244a6acf2b2b06301391fdea4307f71
Carlos Silva <carlos@thehyve.nl>
2015-03-13 15:54:24 +0100

    Removing commented code

--- opal-server/src/test/java/org/obiba/opal/server/DevOpalServer.java
+++ opal-server/src/test/java/org/obiba/opal/server/DevOpalServer.java
@@ -1,11 +1,11 @@
 package org.obiba.opal.server;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import org.obiba.opal.server.httpd.OpalJettyServer;
-import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 //VM args for the launcher: -Xms1G -Xmx2G -XX:MaxPermSize=256M -XX:+UseG1GC
@@ -19,13 +19,6 @@ public class DevOpalServer extends OpalJettyServer {
         setOpalSysProperties();
         OpalServer.setProperties();
         OpalServer.configureSLF4JBridgeHandler();
-
-        //If using slf4j SimpleLogger, use this to change the log level (or set a property externally):
-        //System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info"); // or "debug", "trace", etc.
-
-        //Previous versions used a different logging binding that could be configured like this:
-        //Logger logger = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        //logger.setLevel(Level.INFO);
 
         new UpgradeCommand().execute(); //this is required to initialize some configuration (mandatory later on)
 
