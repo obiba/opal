8135854ba10902fbff4c4fec1892bb84897ef0d7
Carlos Silva <carlos@thehyve.nl>
2015-04-17 15:38:25 +0200

    Adding Expires=-1 to response headers for preventing IE caching

---
+++ opal-core-ws/src/main/java/org/obiba/opal/web/WebContentInterceptor.java
@@ -0,0 +1,24 @@
+package org.obiba.opal.web;
+
+import org.jboss.resteasy.core.ResourceMethodInvoker;
+import org.jboss.resteasy.core.ServerResponse;
+import org.jboss.resteasy.spi.HttpRequest;
+import org.obiba.opal.web.ws.intercept.RequestCyclePostProcess;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collections;
+
+/**
+ *
+ */
+@Component
+public class WebContentInterceptor implements RequestCyclePostProcess {
+
+    @Override
+    public void postProcess(HttpRequest request, ResourceMethodInvoker resourceMethod, ServerResponse response) {
+        MultivaluedMap<String, Object> map = response.getHeaders();
+        //making sure IE doesn't cache: https://support.microsoft.com/en-us/kb/234067
+        map.put("Expires", Collections.<Object>singletonList("-1"));
+    }
+}
