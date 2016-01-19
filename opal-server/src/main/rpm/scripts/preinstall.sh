3573a977b0b26f53820f2d96a641db5bd33bdfb7
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-01-19 12:47:18 -0500

    OPAL-2786 - fixed the home directory in RPM install scripts

--- opal-server/src/main/rpm/scripts/preinstall.sh
+++ opal-server/src/main/rpm/scripts/preinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 getent group adm >/dev/null || groupadd -r adm
-getent passwd opal >/dev/null || \
+
+$(getent passwd opal >/dev/null)
+
+if [ $? != 0 ]; then
     useradd -r -g adm -d /var/lib/opal -s /sbin/nologin \
-    -c "User for Opal" opal
+        -c "User for Opal" opal
+else
+    usermod -d /var/lib/opal opal
+fi
+
 exit 0
