e160d576758a09ce16c2e7099449fbf39d76a8c4
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-01-22 15:37:10 -0500

    OPAL-2786 - fixed install and remove scripts

--- opal-server/src/main/rpm/scripts/preinstall.sh
+++ opal-server/src/main/rpm/scripts/preinstall.sh
@@ -8,7 +8,19 @@ if [ $? != 0 ]; then
     useradd -r -g adm -d /var/lib/opal -s /sbin/nologin \
         -c "User for Opal" opal
 else
-    usermod -d /var/lib/opal opal
+
+  # stop the service if running
+  if service opal status > /dev/null; then
+    if which service >/dev/null 2>&1; then
+      service opal stop
+    elif which invoke-rc.d >/dev/null 2>&1; then
+      invoke-rc.d opal stop
+    else
+      /etc/init.d/opal stop
+    fi
+  fi
+
+  usermod -d /var/lib/opal opal
 fi
 
 exit 0
