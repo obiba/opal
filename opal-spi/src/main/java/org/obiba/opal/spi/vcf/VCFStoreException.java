2c46d8890e890cf5829d3ba371237b05c53e3c79
Yannick Marcon <yannick.marcon@obiba.org>
2017-03-14 08:48:43 +0100

    OPAL-2863 VCF store exception added

---
+++ opal-spi/src/main/java/org/obiba/opal/spi/vcf/VCFStoreException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.opal.spi.vcf;
+
+/**
+ * Exceptions related to VCF Store operations.
+ */
+public class VCFStoreException extends RuntimeException {
+
+  public VCFStoreException(String message) {
+    super(message);
+  }
+
+  public VCFStoreException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
