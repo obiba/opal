01745d44c269b4d9a6806629068d679dda3140b4
Yannick Marcon <yannick.marcon@obiba.org>
2017-07-06 11:12:25 +0200

    OPAL-2938 Automatically unzip plugin archive before including it

--- opal-core/src/main/java/org/obiba/opal/core/runtime/DefaultOpalRuntime.java
+++ opal-core/src/main/java/org/obiba/opal/core/runtime/DefaultOpalRuntime.java
@@ -12,9 +12,11 @@ package org.obiba.opal.core.runtime;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.io.Files;
 import net.sf.ehcache.CacheManager;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
+import org.obiba.core.util.FileUtil;
 import org.obiba.magma.Datasource;
 import org.obiba.magma.MagmaCacheExtension;
 import org.obiba.magma.MagmaEngine;
@@ -28,7 +30,6 @@ import org.obiba.opal.fs.impl.DefaultOpalFileSystem;
 import org.obiba.opal.fs.security.SecuredOpalFileSystem;
 import org.obiba.opal.spi.ServicePlugin;
 import org.obiba.opal.spi.search.SearchServiceLoader;
-import org.obiba.opal.spi.vcf.VCFStoreService;
 import org.obiba.opal.spi.vcf.VCFStoreServiceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,9 +44,14 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.validation.constraints.NotNull;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  *
@@ -329,6 +335,11 @@ public class DefaultOpalRuntime implements OpalRuntime {
     folder.createFolder();
   }
 
+  /**
+   * List valid and most recent version plugins.
+   *
+   * @return
+   */
   private Collection<Plugin> listPlugins() {
     Map<String, Plugin> pluginsMap = Maps.newLinkedHashMap();
     // make sure plugins directory exists
@@ -336,13 +347,119 @@ public class DefaultOpalRuntime implements OpalRuntime {
     // read it to enhance classpath
     File pluginsDir = new File(PLUGINS_DIR);
     if (!pluginsDir.exists() || !pluginsDir.isDirectory() || !pluginsDir.canRead()) return pluginsMap.values();
-    File[] children = pluginsDir.listFiles();
-    if (children == null) return pluginsMap.values();
+    preparePlugins(pluginsDir);
+    addPlugins(pluginsMap, pluginsDir);
+    return pluginsMap.values();
+  }
+
+  /**
+   * Uncompress and archive any zip file that could be found.
+   *
+   * @param pluginsDir
+   */
+  private void preparePlugins(File pluginsDir) {
+    File[] children = pluginsDir.listFiles(pathname -> !pathname.isDirectory() && pathname.getName().endsWith("-dist.zip"));
+    if (children == null || children.length == 0) return;
+    File archiveDir = new File(pluginsDir, ".archive");
+    if (!archiveDir.exists()) archiveDir.mkdirs();
+    for (File child : children) {
+      try {
+        extractPlugin(child);
+        Files.move(child, new File(archiveDir, child.getName()));
+      } catch (IOException e) {
+        log.warn("Failed extracting plugin file: {}" + child.getAbsolutePath(), e);
+      }
+    }
+  }
+
+  /**
+   * Extract plugin folder from zip file.
+   *
+   * @param fileZip
+   * @throws IOException
+   */
+  private void extractPlugin(File fileZip) throws IOException {
+    File destination = new File(fileZip.getParent());
+    File expectedFolder = new File(destination, fileZip.getName().replace("-dist.zip", ""));
+    // backup any site properties
+    File sitePropertiesBackup = backupPluginSiteProperties(expectedFolder);
+    // Open the zip file
+    ZipFile zipFile = new ZipFile(fileZip);
+    Enumeration<?> enu = zipFile.entries();
+    while (enu.hasMoreElements()) {
+      ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+      String name = zipEntry.getName();
+      log.info("Plugin extract: {}", name);
+      // Do we need to create a directory ?
+      File file = new File(destination, name);
+      if (name.endsWith("/")) {
+        file.mkdirs();
+        continue;
+      }
+      // Extract the file
+      InputStream is = zipFile.getInputStream(zipEntry);
+      FileOutputStream fos = new FileOutputStream(file);
+      byte[] bytes = new byte[1024];
+      int length;
+      while ((length = is.read(bytes)) >= 0) {
+        fos.write(bytes, 0, length);
+      }
+      is.close();
+      fos.close();
+    }
+    zipFile.close();
+    // restore site properties
+    restorePluginSiteProperties(expectedFolder, sitePropertiesBackup);
+  }
+
+  /**
+   * Backup site properties file if found and clear old plugin folder.
+   *
+   * @param pluginFolder
+   * @return
+   * @throws IOException
+   */
+  private File backupPluginSiteProperties(File pluginFolder) throws IOException {
+    File sitePropertiesBackup = null;
+    if (pluginFolder.exists()) {
+      File siteProperties = new File(pluginFolder, SITE_PROPERTIES);
+      sitePropertiesBackup = File.createTempFile("site", ".properties");
+      if (siteProperties.exists()) {
+        FileUtil.copyFile(siteProperties, sitePropertiesBackup);
+      }
+      FileUtil.delete(pluginFolder);
+    }
+    return sitePropertiesBackup;
+  }
+
+  /**
+   * Restore any site properties file that would have been backed up.
+   *
+   * @param pluginFolder
+   * @param sitePropertiesBackup
+   * @throws IOException
+   */
+  private void restorePluginSiteProperties(File pluginFolder, File sitePropertiesBackup) throws IOException {
+    if (sitePropertiesBackup == null || !sitePropertiesBackup.exists()) return;
+    File siteProperties = new File(pluginFolder, SITE_PROPERTIES);
+    if (siteProperties.exists()) FileUtil.delete(siteProperties);
+    FileUtil.copyFile(sitePropertiesBackup, siteProperties);
+    sitePropertiesBackup.delete();
+  }
+
+  /**
+   * Discover valid and most recent version plugins.
+   *
+   * @param pluginsMap
+   * @param pluginsDir
+   */
+  private void addPlugins(Map<String, Plugin> pluginsMap, File pluginsDir) {
+    File[] children = pluginsDir.listFiles(pathname -> pathname.isDirectory() && !pathname.getName().startsWith("."));
+    if (children == null || children.length == 0) return;
     for (File child : children) {
       Plugin plugin = new Plugin(child);
       addPlugin(pluginsMap, plugin);
     }
-    return pluginsMap.values();
   }
 
   /**
