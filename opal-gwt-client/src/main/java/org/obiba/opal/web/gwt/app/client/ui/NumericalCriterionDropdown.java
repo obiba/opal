0c02c5feb3661cb7dcaaa3ca2b0226fd7fa1e8f7
Yannick Marcon <yannick.marcon@obiba.org>
2017-11-09 18:40:16 +0100

    OPAL-2958 numerical dropdown and compare js methods to handle value sequences

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/ui/NumericalCriterionDropdown.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/ui/NumericalCriterionDropdown.java
@@ -302,17 +302,30 @@ public abstract class NumericalCriterionDropdown extends ValueSetCriterionDropdo
     statement = "$('" + variable.getName() + "')";
     // RANGE
     if (rangeValueChooser.isItemSelected(0)) {
-      if (min.getText().isEmpty() && max.getText().isEmpty()) {
+      if (min.getText().isEmpty() && max.getText().isEmpty())
         statement = "";
+      else if (variable.getIsRepeatable())
+        statement = statement + ".filter(function(v) { return v";
+
+      if (!min.getText().isEmpty() && !max.getText().isEmpty()) {
+        if (variable.getIsRepeatable())
+          statement = statement + ".ge(" + min.getText() + ").and(v.le(" + max.getText() + "))";
+        else
+          statement = statement + ".ge(" + min.getText() + ").and(" + statement + ".le(" + max.getText() + "))";
       }
-      if (!min.getText().isEmpty() && !max.getText().isEmpty())
-        statement = statement + ".ge(" + min.getText() + ").and(" + statement + ".le(" + max.getText() + "))";
       else if (!min.getText().isEmpty()) statement = statement + ".ge(" + min.getText() + ")";
       else if (!max.getText().isEmpty()) statement = statement + ".le(" + max.getText() + ")";
 
+      if (variable.getIsRepeatable() && !Strings.isNullOrEmpty(statement))
+        statement = statement + " }).empty().not()";
+
       if (((CheckBox) radioControls.getWidget(4)).getValue()) {
-        statement = Strings.isNullOrEmpty(statement) ?
-            "$('" + variable.getName() + "').isNull()" : statement + ".not()";
+        if (Strings.isNullOrEmpty(statement))
+          statement = "$('" + variable.getName() + "').isNull()";
+        else if (variable.getIsRepeatable())
+          statement = statement.replace(".not()", "");
+        else
+          statement = statement + ".not()";
       }
 
       return statement;
@@ -326,10 +339,16 @@ public abstract class NumericalCriterionDropdown extends ValueSetCriterionDropdo
         for (String n : nb.trim().split(","))
           if (!n.trim().isEmpty()) numbers.add(n.trim());
       }
-      statement = statement + ".any(" + Joiner.on(",").join(numbers) + ")";
+      if (numbers.contains("*"))
+        statement = "";
+      else
+        statement = statement + ".any(" + Joiner.on(",").join(numbers) + ")";
 
       if (((CheckBox) radioControls.getWidget(4)).getValue()) {
-        statement = statement + ".not()";
+        if (Strings.isNullOrEmpty(statement))
+          statement = "$('" + variable.getName() + "').isNull()";
+        else
+          statement = statement + ".not()";
       }
 
       return statement;
