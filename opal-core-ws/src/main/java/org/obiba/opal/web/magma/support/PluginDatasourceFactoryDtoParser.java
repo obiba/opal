b9b769a46736cc77d993b805b77417f536d000ba
Yannick Marcon <yannick.marcon@obiba.org>
2018-07-04 17:55:44 +0200

    datasource spi and factory parser added

---
+++ opal-core-ws/src/main/java/org/obiba/opal/web/magma/support/PluginDatasourceFactoryDtoParser.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2018 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.obiba.opal.web.magma.support;
+
+import com.google.common.base.Strings;
+import org.json.JSONObject;
+import org.obiba.magma.DatasourceFactory;
+import org.obiba.magma.datasource.crypt.DatasourceEncryptionStrategy;
+import org.obiba.opal.core.runtime.OpalRuntime;
+import org.obiba.opal.spi.datasource.DatasourceService;
+import org.obiba.opal.spi.datasource.DatasourceUsage;
+import org.obiba.opal.web.model.Magma;
+import org.obiba.opal.web.model.Magma.DatasourceFactoryDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Plugin datasource is used as a transient datasource.
+ */
+@Component
+public class PluginDatasourceFactoryDtoParser extends AbstractDatasourceFactoryDtoParser {
+
+  private static final Logger log = LoggerFactory.getLogger(PluginDatasourceFactoryDtoParser.class);
+
+  private final OpalRuntime opalRuntime;
+
+  @Autowired
+  public PluginDatasourceFactoryDtoParser(OpalRuntime opalRuntime) {
+    this.opalRuntime = opalRuntime;
+  }
+
+  @NotNull
+  @Override
+  protected DatasourceFactory internalParse(DatasourceFactoryDto dto, DatasourceEncryptionStrategy encryptionStrategy) {
+    Magma.PluginDatasourceFactoryDto pluginDto = dto.getExtension(Magma.PluginDatasourceFactoryDto.params);
+    DatasourceService datasourceService = (DatasourceService) opalRuntime.getServicePlugin(pluginDto.getName());
+    JSONObject parameters = null;
+    try {
+      if (pluginDto.hasParameters() && !Strings.isNullOrEmpty(pluginDto.getParameters()))
+        parameters = new JSONObject(pluginDto.getParameters());
+    } catch (Exception e) {
+      log.warn("Cannot parse datasource plugin {} parameters: {}", pluginDto.getName(), pluginDto.getParameters(), e);
+    }
+    return datasourceService.createDatasource(DatasourceUsage.IMPORT, parameters == null ? new JSONObject() : parameters);
+  }
+
+  /**
+   * Verifies the dto type and whether an associated {@link DatasourceService} plugin can be found from the specified name.
+   *
+   * @param dto
+   * @return
+   */
+  @Override
+  public boolean canParse(DatasourceFactoryDto dto) {
+    if (!dto.hasExtension(Magma.PluginDatasourceFactoryDto.params)) return false;
+    Magma.PluginDatasourceFactoryDto pluginDto = dto.getExtension(Magma.PluginDatasourceFactoryDto.params);
+    return opalRuntime.hasServicePlugin(pluginDto.getName()) && opalRuntime.getServicePlugin(pluginDto.getName()) instanceof DatasourceService;
+  }
+
+}
---
+++ opal-spi/src/main/java/org/obiba/opal/spi/datasource/DatasourceUsage.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.opal.spi.datasource;
+
+/**
+ * Enumerated usages of a datasource.
+ */
+public enum DatasourceUsage {
+
+  IMPORT,      // import (meta)data
+  EXPORT,      // export (meta)data
+  //STORAGE,     // store (meta)data
+  //IMPORT_META, // read meta-data
+  //EXPORT_META  // write meta-data
+
+}
