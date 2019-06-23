1a9a363fe2967e7ad2388622128879f73f0ecfd3
Yannick Marcon <yannick.marcon@obiba.org>
2019-06-23 23:47:59 +0200

    Code cleaning

--- opal-r/src/main/resources/.haven_labelled.as.factor.R
+++ opal-r/src/main/resources/.haven_labelled.as.factor.R
@@ -1,12 +1,9 @@
-# transform a haven_labelled vector into a factor with appropritae levels
+# transform a haven_labelled vector into a factor with appropritae levels (can be more than the observed values)
 .haven_labelled.as.factor <- function(x) {
     rval <- `x`
     for (n in colnames(rval)) {
         if ("haven_labelled" %in% class(rval[[n]])) {
             v <- as.factor(rval[[n]])
-            print(n)
-            print(levels(v))
-            print(attributes(rval[[n]])$labels)
             levels(v) <- attributes(rval[[n]])$labels
             rval[[n]] <- v
         }
