db2e0b05a4f2ce373262a421ef858987759539aa
Yannick Marcon <yannick.marcon@obiba.org>
2019-06-24 00:33:19 +0200

    Update copyright

--- opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/search/entity/SearchEntityPresenter.java
+++ opal-gwt-client/src/main/java/org/obiba/opal/web/gwt/app/client/search/entity/SearchEntityPresenter.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Public License v3.0.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.obiba.opal.web.gwt.app.client.search.entity;
