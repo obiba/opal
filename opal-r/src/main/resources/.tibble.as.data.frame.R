588b75f6157b6cbab8a575346a35a919ac5e5b6e
Yannick Marcon <yannick.marcon@obiba.org>
2019-06-24 08:16:05 +0200

    Use factor in place of as.factor and make sure character vectors are not turned into factors

--- opal-r/src/main/resources/.tibble.as.data.frame.R
+++ opal-r/src/main/resources/.tibble.as.data.frame.R
@@ -1,11 +1,9 @@
 # transform a haven_labelled vector into a factor with appropritae levels (can be more than the observed values)
 .tibble.as.data.frame <- function(x) {
-    rval <- as.data.frame(`x`)
+    rval <- as.data.frame(`x`, stringsAsFactors=FALSE)
     for (n in colnames(rval)) {
         if ("haven_labelled" %in% class(rval[[n]])) {
-            v <- as.factor(rval[[n]])
-            levels(v) <- attributes(rval[[n]])$labels
-            rval[[n]] <- v
+            rval[[n]] <- factor(rval[[n]], levels = attributes(rval[[n]])$labels)
         }
     }
     rval
