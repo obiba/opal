645c223086135fdc0d37238dace727b6d2a64ff4
Yannick Marcon <yannick.marcon@obiba.org>
2021-06-03 22:14:52 +0200

    datashield/dsBase#210 Enforce stringsAsFactors to FALSE for R 3.x

--- opal-r/src/main/java/org/obiba/opal/r/rserve/RserveService.java
+++ opal-r/src/main/java/org/obiba/opal/r/rserve/RserveService.java
@@ -411,10 +411,10 @@ public class RserveService implements RServerService, ROperationTemplate {
       // extract DS fields from DESCRIPTION files
       eval("assign('pkgs', Map(function(p) { x <- as.list(p) ; x[names(x) %in% dsFields] }, " +
           "         Filter(function(p) any(names(p) %in% dsFields), " +
-          "                lapply(installed.packages()[,1], function(p) as.data.frame(read.dcf(system.file('DESCRIPTION', package=p)))))))");
+          "                lapply(installed.packages()[,1], function(p) as.data.frame(read.dcf(system.file('DESCRIPTION', package=p)), stringsAsFactors = FALSE)))))");
       // extract DS fields from DATASHIELD files
       eval("assign('x', lapply(installed.packages()[,1], function(p) system.file('DATASHIELD', package=p)))");
-      eval("assign('y', lapply(x[lapply(x, nchar)>0], function(f) as.list(as.data.frame(read.dcf(f)))))");
+      eval("assign('y', lapply(x[lapply(x, nchar)>0], function(f) as.list(as.data.frame(read.dcf(f), stringsAsFactors = FALSE))))");
       // merge and prepare DS field values as arrays of strings
       eval("assign('pkgs', lapply(append(pkgs, y), function(p) lapply(p, function(pp)  gsub('^\\\\s+|\\\\s+$', '', gsub('\\n', '', unlist(strsplit(pp, ',')))))))");
       setResult(eval("pkgs", RSerialize.NATIVE));
