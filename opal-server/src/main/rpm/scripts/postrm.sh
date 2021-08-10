7cbeda4aa5a71ff8bfcc98c44d432217935d18e3
Yannick Marcon <yannick.marcon@obiba.org>
2021-08-10 21:46:52 +0200

    rpm code cleaning

--- opal-server/src/main/rpm/scripts/postrm.sh
+++ opal-server/src/main/rpm/scripts/postrm.sh
@@ -4,28 +4,6 @@
 
 set -e
 
-# summary of how this script can be called:
-#        * <postrm> `remove'
-#        * <postrm> `purge'
-#        * <old-postrm> `upgrade' <new-version>
-#        * <new-postrm> `failed-upgrade' <old-version>
-#        * <new-postrm> `abort-install'
-#        * <new-postrm> `abort-install' <old-version>
-#        * <new-postrm> `abort-upgrade' <old-version>
-#        * <disappearer's-postrm> `disappear' <overwriter>
-#          <overwriter-version>
-# for details, see http://www.debian.org/doc/debian-policy/ or
-# the debian-policy package
-
-case "$1" in
-	0)
-
-    rm -rf /run/opal /var/log/opal /tmp/opal
-  ;;
-esac
-
-exit 0
-
 systemctl daemon-reload >/dev/null 2>&1 || :
 case "$1" in
 	0)
