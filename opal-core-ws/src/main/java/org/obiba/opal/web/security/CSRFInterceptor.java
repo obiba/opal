f79a199ca52cd8ebac089f29f7f271d1485d4efe
Yannick Marcon <yannick.marcon@obiba.org>
2022-02-18 15:45:16 +0100

    #3703 csrf.allowed setting added

--- opal-core-ws/src/main/java/org/obiba/opal/web/security/CSRFInterceptor.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/security/CSRFInterceptor.java
@@ -9,6 +9,9 @@
  */
 package org.obiba.opal.web.security;
 
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.jboss.resteasy.core.ResourceMethodInvoker;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.obiba.opal.web.ws.intercept.RequestCyclePreProcess;
@@ -16,12 +19,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import java.net.URI;
+import java.util.List;
 
 /**
  * Basic CSRF detection.
@@ -39,29 +43,48 @@ public class CSRFInterceptor extends AbstractSecurityComponent implements Reques
 
   private final boolean productionMode;
 
+  private final List<String> csrfAllowed;
+
   @Autowired
   public CSRFInterceptor(@Value("${org.obiba.opal.http.port:8080}") String port,
-                         @Value("${productionMode}") boolean productionMode) {
+                         @Value("${productionMode}") boolean productionMode,
+                         @Value("${csrf.allowed}") String csrfAllowed) {
     serverPort = port;
     this.productionMode = productionMode;
+    this.csrfAllowed = Strings.isNullOrEmpty(csrfAllowed) ? Lists.newArrayList() : Splitter.on(",").splitToList(csrfAllowed.trim());
   }
 
   @Nullable
   @Override
   public Response preProcess(HttpRequest request, ResourceMethodInvoker method) {
+    if (!productionMode || csrfAllowed.contains("*")) return null;
+
     String host = request.getHttpHeaders().getHeaderString(HOST_HEADER);
     String referer = request.getHttpHeaders().getHeaderString(REFERER_HEADER);
     if (referer != null) {
-      String localhost = String.format("localhost:%s", serverPort);
-      boolean forbidden = false;
+      String refererHostPort = "";
+      try {
+        URI refererURI = URI.create(referer);
+        refererHostPort = refererURI.getHost() + (refererURI.getPort() > 0 ? ":" + refererURI.getPort() : "");
+      } catch (Exception e) {
+        // malformed url
+      }
+      // explicitly ok
+      if (csrfAllowed.contains(refererHostPort)) return null;
 
-      if (localhost.equals(host)) {
+      String localhost = String.format("localhost:%s", serverPort);
+      String loopbackhost = String.format("127.0.0.1:%s", serverPort);
+      boolean forbidden = false;
+      if (localhost.equals(host) || loopbackhost.equals(host)) {
         if (!referer.startsWith(String.format("http://%s/", host)))
-          forbidden = productionMode;
-      } else if (!referer.startsWith(String.format("https://%s/", host)))
+          forbidden = true;
+      } else if (!referer.startsWith(String.format("https://%s/", host))) {
         forbidden = true;
+      }
+
       if (forbidden) {
         log.warn("CSRF detection: Host={}, Referer={}", host, referer);
+        log.info(">> You can add {} to csrf.allowed setting", refererHostPort);
         return Response.status(Status.FORBIDDEN).build();
       }
     }
