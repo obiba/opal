cdddf0491e04c1c02e31af118356e2a68f22ee0c
Yannick Marcon <yannick.marcon@obiba.org>
2024-03-31 14:12:24 +0200

    configure polyglot log

--- opal-server/src/main/deb/systemd/start.sh
+++ opal-server/src/main/deb/systemd/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$JAVA $JAVA_ARGS -cp "${OPAL_HOME}/conf:${OPAL_DIST}/lib/*" -DOPAL_HOME=${OPAL_HOME} -DOPAL_DIST=${OPAL_DIST} -DOPAL_LOG=${OPAL_LOG} org.obiba.opal.server.OpalServer $OPAL_ARGS
+$JAVA $JAVA_ARGS -cp "${OPAL_HOME}/conf:${OPAL_DIST}/lib/*" -DOPAL_HOME=${OPAL_HOME} -DOPAL_DIST=${OPAL_DIST} -DOPAL_LOG=${OPAL_LOG} -Dpolyglot.log.file=${OPAL_LOG}/polyglot.log -Dpolyglot.engine.WarnInterpreterOnly=false org.obiba.opal.server.OpalServer $OPAL_ARGS
