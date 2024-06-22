85a4fb539f2c2d2b9d2af06d5a4e024939e97a36
Yannick Marcon <yannick.marcon@obiba.org>
2024-06-22 15:07:52 +0200

    feat(ui): added datashield logs download

--- opal-ui/src/pages/AdminDatashieldPage.vue
+++ opal-ui/src/pages/AdminDatashieldPage.vue
@@ -11,11 +11,39 @@
       <div class="text-h5 q-mb-md">{{ $t('packages') }}</div>
       <div class="text-help q-mb-md">{{ $t('datashield.packages_info') }}</div>
       <datashield-clusters />
-      <div class="text-h5 q-mb-md">{{ $t('permissions') }}</div>
-      <access-control-list
-        resource="/system/permissions/datashield"
-        :options="['DATASHIELD_USE', 'DATASHIELD_ALL']"
-      />
+      <div class="row q-col-gutter-md">
+        <div class="col-12 col-md-6">
+          <div class="text-h5 q-mb-md">{{ $t('permissions') }}</div>
+          <access-control-list
+            resource="/system/permissions/datashield"
+            :options="['DATASHIELD_USE', 'DATASHIELD_ALL']"
+          />
+        </div>
+        <div class="col-12 col-md-6">
+          <div class="text-h5 q-mb-md">{{ $t('datashield.audit') }}</div>
+          <div class="text-help q-mb-md">{{ $t('datashield.audit_info') }}</div>
+          <q-btn-dropdown
+            :label="$t('download')"
+            icon="download"
+            size="sm"
+            color="primary"
+            >
+            <q-list>
+              <q-item clickable v-close-popup @click="onDownloadLogs(true)">
+                <q-item-section>
+                  <q-item-label>{{ $t('datashield.download_all_logs') }}</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="onDownloadLogs(false)">
+                <q-item-section>
+                  <q-item-label>{{ $t('datashield.download_latest_logs') }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </div>
+      </div>
+
       <div class="text-h5 q-mb-md">{{ $t('profiles') }}</div>
       <div class="text-help q-mb-md">{{ $t('datashield.profiles_info') }}</div>
       <datashield-profiles />
@@ -24,6 +52,7 @@
 </template>
 
 <script setup lang="ts">
+import { baseUrl } from 'src/boot/api';
 import DatashieldClusters from 'src/components/admin/datashield/DatashieldClusters.vue';
 import DatashieldProfiles from 'src/components/admin/datashield/DatashieldProfiles.vue';
 import AccessControlList from 'src/components/permissions/AccessControlList.vue';
@@ -35,4 +64,8 @@ onMounted(() => {
   rStore.initR();
   datashieldStore.initProfiles();
 });
+
+function onDownloadLogs(all: boolean) {
+  window.open(`${baseUrl}/system/log/datashield.log?all=${all}`);
+}
 </script>
