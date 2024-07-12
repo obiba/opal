7be23474a243e96cc146aac6ece6d7416f3b84b1
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2024-07-11 20:23:51 -0400

    Completed the add token dialogs, added dup token exp

--- opal-ui/src/components/admin/profiles/user/AddTokenDialog.vue
+++ opal-ui/src/components/admin/profiles/user/AddTokenDialog.vue
@@ -20,6 +20,7 @@
           </q-input>
           <q-input
             type="text"
+            readonly
             :label="$t('token') + ' *'"
             :hint="$t('user_profile.token_dialog.name_hint')"
             v-model="token.token"
@@ -37,11 +38,43 @@
             use-chips
             multiple
             input-debounce="0"
+            :label="$t('projects')"
             :options="projectFilters"
             @new-value="addProject"
             @filter="filterProjects"
           >
           </q-select>
+
+          <add-token-dialog-access-section
+            v-if="showAccessTasks"
+            v-model="token.access"
+          ></add-token-dialog-access-section>
+
+          <add-token-dialog-option-groups
+            v-if="showAccessTasks"
+            v-model="token.commands"
+            :group-options="taskGroupOptions"
+            title="user_profile.token_dialog.project_tasks"
+            hint="user_profile.token_dialog.project_tasks_hint"
+          ></add-token-dialog-option-groups>
+
+          <add-token-dialog-option-groups
+            v-if="showAdminOptions"
+            v-model="tokenAdmin"
+            :group-options="adminGroupOptions"
+            title="user_profile.token_dialog.project_administration"
+            hint="user_profile.token_dialog.project_administration_hint"
+            @update:model-value="onTokenAdminUpdate"
+          ></add-token-dialog-option-groups>
+
+          <add-token-dialog-option-groups
+            v-if="showAdminOptions"
+            v-model="tokenServices"
+            :group-options="servicesGroupOptions"
+            title="user_profile.token_dialog.services"
+            hint="user_profile.token_dialog.services_hint"
+            @update:model-value="onTokenServicesUpdate"
+          ></add-token-dialog-option-groups>
         </q-form>
       </q-card-section>
       <q-separator />
@@ -66,6 +99,9 @@ import { SubjectTokenDto /*, ProjectDto*/ } from 'src/models/Opal';
 import { notifyError } from 'src/utils/notify';
 import { generateToken } from 'src/utils/tokens';
 import { generateName } from 'src/utils/strings';
+import { TOKEN_TYPES } from 'src/stores/tokens';
+import AddTokenDialogAccessSection from './AddTokenDialogAccessSection.vue';
+import AddTokenDialogOptionGroups from './AddTokenDialogOptionGroups.vue';
 
 interface DialogProps {
   modelValue: boolean;
@@ -79,9 +115,12 @@ const formRef = ref();
 const emit = defineEmits(['update:modelValue']);
 const tokensStore = useTokensStore();
 const projectsStore = useProjectsStore();
-
 let projectsFilterOptions = Array<string>();
 const projectFilters = ref(Array<string>());
+const tokenAdmin = ref<string[]>([]);
+const tokenServices = ref<string[]>([]);
+const showAccessTasks = computed(() => [TOKEN_TYPES.R, TOKEN_TYPES.CUSTOM].includes(props.type as TOKEN_TYPES));
+const showAdminOptions = computed(() => TOKEN_TYPES.CUSTOM === props.type);
 
 const emptyToken: SubjectTokenDto = {
   name: '',
@@ -106,11 +145,56 @@ watch(
       token.value.name = generateName(props.type, props.names);
       updateTokenType();
       showDialog.value = value;
+      initializeTaskGroupOptions();
+      initializeAdminGroupOptions();
+      initializeServicesGroupOptions();
     }
   }
 );
 
+// Validations
 const validateRequiredName = (val: string) => (val && val.trim().length > 0) || t('validation.token.name_required');
+
+// Group options
+const taskGroupOptions: { label: string; value: string }[] = [];
+const adminGroupOptions: { label: string; value: string }[] = [];
+const servicesGroupOptions: { label: string; value: string }[] = [];
+
+function initializeTaskGroupOptions() {
+  taskGroupOptions.push(
+    { label: t('command_types.import'), value: 'import' },
+    { label: t('command_types.export'), value: 'export' }
+  );
+
+  if (props.type === TOKEN_TYPES.CUSTOM) {
+    taskGroupOptions.push(
+      { label: t('command_types.copy'), value: 'copy' },
+      { label: t('command_types.analyse'), value: 'analyse' },
+      { label: t('command_types.report'), value: 'report' },
+      { label: t('command_types.import_vcf'), value: 'import_vcf' },
+      { label: t('command_types.export_vcf'), value: 'export_vcf' },
+      { label: t('command_types.backup'), value: 'backup' },
+      { label: t('command_types.restore'), value: 'restore' }
+    );
+  }
+}
+
+function initializeAdminGroupOptions() {
+  adminGroupOptions.push(
+    { label: t('token_administration.createProject'), value: 'createProject' },
+    { label: t('token_administration.updateProject'), value: 'updateProject' },
+    { label: t('token_administration.deleteProject'), value: 'deleteProject' }
+  );
+}
+
+function initializeServicesGroupOptions() {
+  servicesGroupOptions.push(
+    { label: t('token_services.useR'), value: 'useR' },
+    { label: t('token_services.useDatashield'), value: 'useDatashield' },
+    { label: t('token_services.useSQL'), value: 'useSQL' },
+    { label: t('token_services.sysAdmin'), value: 'sysAdmin' }
+  );
+}
 
 function updateTokenType() {
   switch (props.type) {
@@ -135,6 +219,19 @@ function filterProjects(val: string, update: any) {
       projectFilters.value = projectFilters.value.filter((v) => v.toLowerCase().indexOf(needle) > -1);
     }
   });
+}
+
+function onTokenAdminUpdate() {
+  token.value.createProject = tokenAdmin.value.includes('createProject');
+  token.value.updateProject = tokenAdmin.value.includes('updateProject');
+  token.value.deleteProject = tokenAdmin.value.includes('deleteProject');
+}
+
+function onTokenServicesUpdate() {
+  token.value.useR = tokenServices.value.includes('useR');
+  token.value.useDatashield = tokenServices.value.includes('useDatashield');
+  token.value.useSQL = tokenServices.value.includes('useSQL');
+  token.value.sysAdmin = tokenServices.value.includes('sysAdmin');
 }
 
 function onCopyToClipboard() {
@@ -165,6 +262,12 @@ function onHide() {
   showDialog.value = false;
   projectsFilterOptions = [];
   projectFilters.value = [];
+  taskGroupOptions.splice(0, taskGroupOptions.length);
+  adminGroupOptions.splice(0, adminGroupOptions.length);
+  tokenAdmin.value = [];
+  servicesGroupOptions.splice(0, servicesGroupOptions.length);
+  tokenServices.value = [];
+
   emit('update:modelValue', false);
 }
 
---
+++ opal-ui/src/components/admin/profiles/user/AddTokenDialogOptionGroups.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row q-mt-lg">
+      <div>
+        <span class="text-subtitle1" :class="{ 'text-primary': group.length > 0, 'text-secondary': group.length < 1 }">{{
+          $t(title)
+        }}</span>
+
+        <q-option-group dense :options="groupOptions" type="checkbox" v-model="group" />
+        <span class="text-caption text-secondary">{{ $t(hint) }}</span>
+      </div>
+    </div>
+  </template>
+  access
+
+  <script lang="ts">
+  export default defineComponent({
+    name: 'AddTokenDialogOptionGroups.vue',
+  });
+  </script>
+
+  <script setup lang="ts">
+
+  interface GroupProps {
+    modelValue: string[] | undefined;
+    groupOptions: { [props: string]: any; label: string; value: any; disable?: boolean | undefined; }[];
+    title: string;
+    hint: string;
+  }
+
+  const props = defineProps<GroupProps>();
+  const emit = defineEmits(['update:modelValue']);
+
+  const group = computed({
+    get: () => props.modelValue || [],
+    set: (value) => emit('update:modelValue', value),
+  });
+  </script>
