1b8487bc774a9e5572b26f363ef37fd3e2deb7c7
ymarcon <yannick.marcon@epfl.ch>
2024-09-27 14:10:38 +0200

    fix: case there is no R activity to show

--- opal-ui/src/components/admin/profiles/RActivity.vue
+++ opal-ui/src/components/admin/profiles/RActivity.vue
@@ -1,39 +1,40 @@
 <template>
   <div>
-    <div class="q-mb-md">
-      <span class="text-hint">{{ $t('total_execution_time') }}:</span>
-      <span class="text-caption q-ml-xs">{{ getMillisLabel(totalExecutionTime) }}</span>
+    <div v-if="profileActivityStore.summaries.length">
+      <div class="q-mb-md">
+        <span class="text-hint">{{ $t('total_execution_time') }}:</span>
+        <span class="text-caption q-ml-xs">{{ getMillisLabel(totalExecutionTime) }}</span>
+      </div>
+      <q-table
+        flat
+        :rows="profileActivityStore.summaries"
+        :columns="columns"
+        :row-key="(row) => `${row.context}:${row.profile}`"
+        >
+        <template v-slot:body-cell-profile="props">
+          <q-td :props="props" @mouseover="onOverRow(props.row)" @mouseleave="onLeaveRow(props.row)">
+            <q-chip :label="props.value" class="q-ma-none" />
+            <div class="float-right">
+              <q-btn
+                rounded
+                dense
+                flat
+                size="sm"
+                color="secondary"
+                :icon="toolsVisible[getKey(props.row)] ? 'visibility' : 'none'"
+                class="q-ml-xs"
+                @click="onShowActivities(props.row)"
+              />
+            </div>
+          </q-td>
+        </template>
+        <template v-slot:body-cell-context="props">
+          <q-td :props="props" @mouseover="onOverRow(props.row)" @mouseleave="onLeaveRow(props.row)">
+            <span class="text-caption">{{ props.value }}</span>
+          </q-td>
+        </template>
+      </q-table>
     </div>
-    <q-table
-      v-if="profileActivityStore.summaries.length"
-      flat
-      :rows="profileActivityStore.summaries"
-      :columns="columns"
-      :row-key="(row) => `${row.context}:${row.profile}`"
-      >
-      <template v-slot:body-cell-profile="props">
-        <q-td :props="props" @mouseover="onOverRow(props.row)" @mouseleave="onLeaveRow(props.row)">
-          <q-chip :label="props.value" class="q-ma-none" />
-          <div class="float-right">
-            <q-btn
-              rounded
-              dense
-              flat
-              size="sm"
-              color="secondary"
-              :icon="toolsVisible[getKey(props.row)] ? 'visibility' : 'none'"
-              class="q-ml-xs"
-              @click="onShowActivities(props.row)"
-            />
-          </div>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-context="props">
-        <q-td :props="props" @mouseover="onOverRow(props.row)" @mouseleave="onLeaveRow(props.row)">
-          <span class="text-caption">{{ props.value }}</span>
-        </q-td>
-      </template>
-    </q-table>
     <div v-else class="text-hint">
       {{ $t('no_r_activity') }}
     </div>
