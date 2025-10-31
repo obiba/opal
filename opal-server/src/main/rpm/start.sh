30b61883c50ed9ccef259e9e7c2454ef0a15f5a1
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2025-10-31 05:01:21 -0400

    Add exec to startup scripts for graceful Java shutdown and OrientDB cleanup (#4067)
    Co-authored-by: Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>

--- opal-server/src/main/rpm/start.sh
+++ opal-server/src/main/rpm/start.sh
@@ -28,4 +28,4 @@ done < <(find "${OPAL_HOME}/plugins/" -type d -name lib | grep -v ".archive")
 
 APP_OPTS="${JAVA_ARGS} -cp ${CLASSPATH} -DOPAL_HOME=${OPAL_HOME} -DOPAL_DIST=${OPAL_DIST} -DOPAL_LOG=${OPAL_LOG} -Dpolyglot.log.file=${OPAL_LOG}/polyglot.log -Dpolyglot.engine.WarnInterpreterOnly=false"
 $JAVA $APP_OPTS org.obiba.opal.server.OpalServer --upgrade
-$JAVA $APP_OPTS org.obiba.opal.server.OpalServer
+exec $JAVA $APP_OPTS org.obiba.opal.server.OpalServer
