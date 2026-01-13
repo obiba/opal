f852cd8a2c1b3d1ecd80d2f50f19f4f76f37b887
Yannick Marcon <yannick.marcon@obiba.org>
2026-01-13 13:20:03 +0100

    feat: include public url if defined in opal config as openapi server

--- opal-core-ws/src/main/java/org/obiba/opal/web/swagger/SwaggerResource.java
+++ opal-core-ws/src/main/java/org/obiba/opal/web/swagger/SwaggerResource.java
@@ -9,6 +9,9 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
+import org.jetbrains.annotations.NotNull;
+import org.obiba.opal.core.service.OpalGeneralConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -17,6 +20,13 @@ import java.util.List;
 
 @Component
 public class SwaggerResource extends io.swagger.v3.jaxrs2.integration.resources.OpenApiResource {
+
+  private final OpalGeneralConfigService opalGeneralConfigService;
+
+  @Autowired
+  public SwaggerResource(OpalGeneralConfigService opalGeneralConfigService) {
+    this.opalGeneralConfigService = opalGeneralConfigService;
+  }
 
   @GET
   @Produces("application/json")
@@ -27,16 +37,34 @@ public class SwaggerResource extends io.swagger.v3.jaxrs2.integration.resources.
     OpenAPI openAPI = io.swagger.v3.core.util.Json.mapper().readValue(openAPIStr, OpenAPI.class);
 
     // Add server information explicitly, otherwise the /ws prefix is missing in the generated spec
-    URI baseUri = uriInfo.getBaseUri();
-    List<Server> servers = new ArrayList<>();
-    Server server = new Server();
-    server.setUrl(baseUri.toString());
-    server.setDescription("OPAL API Server");
-    servers.add(server);
+    List<Server> servers = getServers(uriInfo);
     openAPI.setServers(servers);
 
     // Dump the modified OpenAPI object back to a string
     openAPIStr = io.swagger.v3.core.util.Json.mapper().writeValueAsString(openAPI);
     return Response.ok(openAPIStr).build();
+  }
+
+  @NotNull
+  private List<Server> getServers(UriInfo uriInfo) {
+    List<Server> servers = new ArrayList<>();
+
+    String publicUrl = opalGeneralConfigService.getConfig().getPublicUrl();
+    if(publicUrl != null && !publicUrl.isEmpty()) {
+      Server server = new Server();
+      server.setUrl(publicUrl + (publicUrl.endsWith("/") ? "" : "/") + "ws/");
+      server.setDescription("OPAL Public API Server");
+      servers.add(server);
+    }
+
+    URI baseUri = uriInfo.getBaseUri();
+    if (publicUrl == null || !baseUri.toString().startsWith(publicUrl)) {
+      Server server = new Server();
+      server.setUrl(baseUri.toString());
+      server.setDescription("OPAL API Server");
+      servers.add(server);
+    }
+
+    return servers;
   }
 }
